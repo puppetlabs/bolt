e913eaba38d832366aa5d06fa2b5d5af1c0bd8df
Patrick Carlisle <patrick.carlisle@puppet.com>
2017-08-11 11:30:56 -0700

    (TASKS-14) Add spec helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,31 @@
+# See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+RSpec.configure do |config|
+
+  # rspec-expectations config
+  config.expect_with :rspec do |expectations|
+    #     be_bigger_than(2).and_smaller_than(4).description
+    #     # => "be bigger than 2 and smaller than 4"
+    # ...rather than:
+    #     # => "be bigger than 2"
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  # rspec-mocks config
+  config.mock_with :rspec do |mocks|
+    # Prevents you from mocking or stubbing a method that does not exist on
+    # a real object.
+    mocks.verify_partial_doubles = true
+  end
+
+  # This will be default in future rspec, leave it on
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  # Allows RSpec to persist some state between runs in order to support
+  # the `--only-failures` and `--next-failure` CLI options.
+  config.example_status_persistence_file_path = "spec/examples.txt"
+
+  config.disable_monkey_patching!
+
+  config.warnings = true
+end
