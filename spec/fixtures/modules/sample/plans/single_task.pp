c7c5a13c936771dcfa55f0f7ad35e911f2acf9ab
Eric Sorenson <eric.sorenson@puppet.com>
2017-09-21 10:13:29 -0700

    Updated single_task to show the new method syntax

--- spec/fixtures/modules/sample/plans/single_task.pp
+++ spec/fixtures/modules/sample/plans/single_task.pp
@@ -1,11 +1,7 @@
 # one line plan to show we can run a task by name
 plan sample::single_task($nodes = String) {
   run_task (
-      Sample::Echo (
-      { 
-        message => "hi there"
-      }
-    ), 
-  $nodes
+    "sample::echo", $nodes,
+    message => "hi there"
   )
 }
