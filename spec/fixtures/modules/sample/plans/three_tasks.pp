42f5eb50a3d5e280d90e2d86fe233a9904790af3
Eric Sorenson <eric.sorenson@puppet.com>
2017-09-21 11:37:07 -0700

    Fixed the two and three task plans
    The plans now run actual sample tasks instead of bogus ones

--- spec/fixtures/modules/sample/plans/three_tasks.pp
+++ spec/fixtures/modules/sample/plans/three_tasks.pp
@@ -1,21 +1,15 @@
-plan app::deployment (
-  String $app_version
-) {
+plan sample::three_tasks($nodes) {
 
-  # disable app hosts in $tier from lb
-  run_task(gce::lb_disable, $nodes["loadbalancers"],
-    real_servers => $nodes["app_hosts"],
+  run_task(sample::echo, $nodes,
+    message => "first task",
   )
 
-  # deploy the app!
-  run_task(app::deploy, $nodes["app_hosts"],
-    version => $app_version,
+  run_task(sample::echo, $nodes,
+    message => "second task",
   )
 
-  # revisit the load balancers and re-enable the app hosts 
-  run_task(gce::lb_enable, $nodes["loadbalancers"],
-    real_servers => $nodes["app_hosts"],    
+  run_task(sample::echo, $nodes,
+    message => "third task",
   )
 
-}  
-
+}
