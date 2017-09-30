1c2dad78d20d4f64f03de90fda4bd580668e68fe
Eric Sorenson <eric.sorenson@puppet.com>
2017-09-30 10:26:02 -0700

    Updated plans to current (but still bad) spec
    The previous plan examples had an error in the signature
    ("$nodes = String" is the wrong syntax, should be "String $nodes")
    and didn't accept different targets for the different tasks
    in the  multi-task plans.
    
    The current code works but is not yet the final form, see BOLT-97

--- spec/fixtures/modules/sample/plans/three_tasks.pp
+++ spec/fixtures/modules/sample/plans/three_tasks.pp
@@ -1,14 +1,18 @@
-plan sample::three_tasks($nodes) {
+plan sample::three_tasks(String $first_targets, String $second_targets, String $third_targets) {
 
-  run_task(sample::echo, $nodes,
+  $first_array = split($first_targets, ',')
+  $second_array = split($second_targets, ',')
+  $third_array = split($third_targets, ',')
+
+  run_task(sample::echo, $first_array,
     message => "first task",
   )
 
-  run_task(sample::echo, $nodes,
+  run_task(sample::echo, $second_array,
     message => "second task",
   )
 
-  run_task(sample::echo, $nodes,
+  run_task(sample::echo, $third_array,
     message => "third task",
   )
 
