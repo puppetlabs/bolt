cc143c2fc1ce3ec38d39336226415c38de2a8ee9
Eric Sorenson <eric.sorenson@puppet.com>
2017-09-30 10:26:02 -0700

    Updated plans to current (but still bad) spec
    The previous plan examples had an error in the signature
    ("$nodes = String" is the wrong syntax, should be "String $nodes")
    and didn't accept different targets for the different tasks
    in the  multi-task plans.
    
    The current code works but is not yet the final form, see BOLT-97

--- spec/fixtures/modules/sample/plans/single_task.pp
+++ spec/fixtures/modules/sample/plans/single_task.pp
@@ -1,7 +1,8 @@
 # one line plan to show we can run a task by name
-plan sample::single_task($nodes = String) {
+plan sample::single_task(String $nodes) {
+  $node_array = split($nodes, ',')
   run_task (
-    "sample::echo", $nodes,
+    "sample::echo", $node_array,
     message => "hi there"
   )
 }
--- spec/fixtures/modules/sample/plans/two_tasks.pp
+++ spec/fixtures/modules/sample/plans/two_tasks.pp
@@ -1,10 +1,13 @@
-plan sample::two_tasks($nodes) {
+plan sample::two_tasks(String $first_targets, String $second_targets) {
 
-  run_task(sample::echo, $nodes,
+  $first_array = split($first_targets, ',')
+  $second_array = split($second_targets, ',')
+
+  run_task(sample::echo, $first_array,
     message => "first task",
   )
 
-  run_task(sample::echo, $nodes,
+  run_task(sample::echo, $second_array,
     message => "second task",
   )
 
