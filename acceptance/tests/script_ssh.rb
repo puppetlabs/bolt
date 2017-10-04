c6eedc2c571379b746ce6e9106c2bd1cb07a00be
John Duarte <john.duarte@puppetlabs.com>
2017-10-03 17:02:11 -0700

    (BOLT-162) Add script tests

---
+++ acceptance/tests/script_ssh.rb
@@ -0,0 +1,36 @@
+test_name "C100548: \
+           bolt script run should execute script on remote hosts via ssh" do
+
+  ssh_nodes = select_hosts(roles: ['ssh'])
+  skip_test('no applicable nodes to test on') if ssh_nodes.empty?
+
+  script = "C100548.sh"
+
+  step "create script on bolt controller" do
+    create_remote_file(bolt, script, <<-FILE)
+    #!/bin/sh
+    echo "hello from $(hostname)"
+    FILE
+  end
+
+  step "execute `bolt script run` via SSH" do
+    user = ENV['SSH_USER']
+    password = ENV['SSH_PASSWORD']
+    nodes_csv = ssh_nodes.map(&:hostname).join(',')
+    bolt_command = "bolt script run #{script} \
+                    --nodes #{nodes_csv}      \
+                    -u #{user} -p #{password} \
+                    --insecure"
+
+    result = nil
+    case bolt['platform']
+    when /windows/
+      result = execute_powershell_script_on(bolt, bolt_command)
+    else
+      result = on(bolt, bolt_command)
+    end
+    ssh_nodes.each do |node|
+      assert_match(/hello from #{node.hostname.split('.')[0]}/, result.stdout)
+    end
+  end
+end
---
+++ acceptance/tests/script_winrm.rb
@@ -0,0 +1,35 @@
+test_name "C100549: \
+           bolt script run should execute script on remote hosts via winrm" do
+
+  winrm_nodes = select_hosts(roles: ['winrm'])
+  skip_test('no applicable nodes to test on') if winrm_nodes.empty?
+
+  script = "C100549.ps1"
+
+  step "create script on bolt controller" do
+    create_remote_file(bolt, script, <<-FILE)
+    [System.Net.Dns]::GetHostByName(($env:computerName))
+    FILE
+  end
+
+  step "execute `bolt script run` via WinRM" do
+    user = ENV['WINRM_USER']
+    password = ENV['WINRM_PASSWORD']
+    nodes_csv = winrm_nodes.map { |host| "winrm://#{host.hostname}" }.join(',')
+    bolt_command = "bolt script run #{script} \
+                    --nodes #{nodes_csv}      \
+                    -u #{user} -p #{password}"
+
+    result = nil
+    case bolt['platform']
+    when /windows/
+      result = on(bolt, powershell(bolt_command))
+    else
+      result = on(bolt, bolt_command)
+    end
+    winrm_nodes.each do |node|
+      assert_match(/#{node.hostname.split('.')[0]}/, result.stdout)
+      assert_match(/{#{node.ip}}/, result.stdout)
+    end
+  end
+end
