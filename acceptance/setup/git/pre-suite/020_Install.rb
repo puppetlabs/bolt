5dd79264d4a672ab1fd2e1f6fd225486bb7bfb07
John Duarte <john.duarte@puppetlabs.com>
2017-10-11 14:21:24 -0700

    (BOLT-162) Acceptance - add SHA to rake task
    This commit adds `GIT_SHA` as a supported ENV_VAR for the `ci:test:git`
    rake task. This value is used to check out that SHA in git and validate
    that the expected version of bolt was installed.

--- acceptance/setup/git/pre-suite/020_Install.rb
+++ acceptance/setup/git/pre-suite/020_Install.rb
@@ -1,28 +1,50 @@
 test_name "Install Bolt via git" do
+  sha = ''
+  version = ''
   step "Clone repo" do
-    on(bolt, "git clone https://github.com/puppetlabs/bolt.git bolt")
+    on(bolt, "git clone #{ENV['GIT_SERVER']}/#{ENV['GIT_FORK']} bolt")
+    if ENV['GIT_SHA'].length > 0 then
+      on(bolt, "cd bolt && git checkout #{ENV['GIT_SHA']}")
+    else
+      on(bolt, "cd bolt && git checkout #{ENV['GIT_BRANCH']}")
+    end
   end
   step "Update submodules" do
     on(bolt, "cd bolt && git submodule update --init --recursive")
   end
-  step "Use fake version" do
+  step "Construct version based on SHA" do
+    if ENV['GIT_SHA'].length > 0 then
+      sha = ENV['GIT_SHA']
+    else
+      sha = on(bolt, 'cd bolt && git rev-parse --short HEAD').stdout.chomp
+    end
+    version = "9.9.#{sha}"
     create_remote_file(bolt, 'bolt/lib/bolt/version.rb', <<-VERS)
     module Bolt
-      VERSION = '9.9.9'.freeze
+      VERSION = '#{version}'.freeze
     end
     VERS
   end
   step "Build gem" do
-    on(bolt, "cd bolt && gem build bolt.gemspec")
+    case bolt['platform']
+    when /windows/
+      execute_powershell_script_on(bolt, 'cd bolt; gem build bolt.gemspec')
+    else
+      on(bolt, "cd bolt && gem build bolt.gemspec")
+    end
   end
   step "Install custom gem" do
-    on(bolt, "cd bolt && gem install bolt-9.9.9.gem")
+    case bolt['platform']
+    when /windows/
+      execute_powershell_script_on(bolt, "cd bolt; gem install bolt-#{version}.gem")
+    else
+      on(bolt, "cd bolt && gem install bolt-#{version}.gem")
+    end
   end
   step "Ensure install succeeded" do
     cmd = 'bolt --version'
     case bolt['platform']
     when /windows/
-      execute_powershell_script_on(bolt, install_command)
       result = on(bolt, powershell(cmd))
     when /osx/
       env = 'source /etc/profile  ~/.bash_profile ~/.bash_login ~/.profile && '
@@ -30,6 +52,6 @@ test_name "Install Bolt via git" do
     else
       result = on(bolt, cmd)
     end
-    assert_match(/9\.9\.9/, result.stdout)
+    assert_match(/#{version}/, result.stdout)
   end
 end
