e4c0d918b2b76bdb39eee87b3fd2ab20b19dc32c
Nick Lewis <nick@puppetlabs.com>
2017-12-11 16:54:30 -0800

    (maint) Use proper block syntax in expectation helper
    This block was intended to be passed to `raise_error`, so that it can
    receive the error object and make assertions on it. It was instead being
    passed to `to`, which just ignored it, causing those expectations to
    never execute.

--- spec/lib/bolt_spec/errors.rb
+++ spec/lib/bolt_spec/errors.rb
@@ -3,11 +3,11 @@ module BoltSpec
     def expect_node_error(klass, issue_code, message)
       expect {
         yield
-      }.to raise_error do |ex|
+      }.to(raise_error { |ex|
         expect(ex).to be_a(klass)
         expect(ex.issue_code).to eq(issue_code)
         expect(ex.message).to match(message)
-      end
+      })
     end
   end
 end
