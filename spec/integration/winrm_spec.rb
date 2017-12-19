2c7faed96df3e2c945ccec260ea3c5e8cd0834e7
Michael Smith <michael.smith@puppet.com>
2017-12-19 12:01:45 -0800

    (BOLT-119) Add integration tests for noop

--- spec/integration/winrm_spec.rb
+++ spec/integration/winrm_spec.rb
@@ -27,6 +27,16 @@ describe "when runnning over the winrm transport", winrm: true do
       result = run_one_node(%W[task run #{stdin_task} message=somemessage] + config_flags)
       expect(result['_output'].strip).to match(/STDIN: {"messa/)
     end
+
+    it 'passes noop to a task that supports noop', reset_puppet_settings: true do
+      result = run_one_node(%w[task run sample::ps_noop message=somemessage --noop] + config_flags)
+      expect(result['_output'].strip).to eq("somemessage with noop true")
+    end
+
+    it 'does not pass noop to a task by default', reset_puppet_settings: true do
+      result = run_one_node(%w[task run sample::ps_noop message=somemessage] + config_flags)
+      expect(result['_output'].strip).to eq("somemessage with noop")
+    end
   end
 
   context 'when using a configfile' do
