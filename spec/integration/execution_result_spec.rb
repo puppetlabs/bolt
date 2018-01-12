1fd093d93bdf20d2a437187fdfe0ee3fbd0de578
Michael Smith <michael.smith@puppet.com>
2018-01-12 12:11:11 -0800

    (maint) Fix plan integration test
    Previously it failed because the password wasn't being set and the task
    was broken. Fix both and add a positive test so the test now verifiably
    works.

--- spec/integration/execution_result_spec.rb
+++ spec/integration/execution_result_spec.rb
@@ -3,20 +3,24 @@ require 'bolt_spec/files'
 require 'bolt_spec/integration'
 require 'bolt/cli'
 
-describe "when runnning over the ssh transport", ssh: true do
+describe "when running a plan that manipulates an execution result", ssh: true do
   include BoltSpec::Conn
   include BoltSpec::Files
   include BoltSpec::Integration
 
   let(:modulepath) { File.join(__dir__, '../fixtures/modules') }
-  let(:uri) { conn_uri('ssh') }
-  let(:user) { conn_info('ssh')[:user] }
-  let(:password) { conn_info('ssh')[:password] }
+  let(:uri) { conn_uri('ssh', true) }
 
   after(:each) { Puppet.settings.send(:clear_everything_for_tests) }
 
   context 'when using CLI options' do
     let(:config_flags) { %W[--insecure --format json --modulepath #{modulepath}] }
+
+    it 'returns true on success' do
+      params = { target: uri }.to_json
+      output = run_cli(['plan', 'run', 'results::test_methods', "--params", params] + config_flags)
+      expect(output.strip).to eq('true')
+    end
 
     it 'returns false on failure' do
       params = { target: uri, fail: true }.to_json
