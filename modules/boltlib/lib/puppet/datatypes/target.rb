75ea7c9b042441303bd662aa0ada9d5c3c071b95
Alex Dreyer <alex@puppetlabs.com>
2018-02-16 11:30:40 -0800

    (BOLT-342) Expose url parts on Target Datatype

--- modules/boltlib/lib/puppet/datatypes/target.rb
+++ modules/boltlib/lib/puppet/datatypes/target.rb
@@ -5,7 +5,12 @@ Puppet::DataTypes.create_type('Target') do
       options => { type => Hash[String[1], Data], value => {} }
     },
     functions => {
+      host => Callable[[], String[1]],
       name => Callable[[], String[1]],
+      password => Callable[[], Optional[String[1]]],
+      port => Callable[[], Optional[Integer]],
+      protocol => Callable[[], Optional[String[1]]],
+      user => Callable[[], Optional[String[1]]],
     }
     PUPPET
 
