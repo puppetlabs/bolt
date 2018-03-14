a72dc0be567131d6b63fb946002b9c4a9afa994c
Alex Dreyer <alex@puppetlabs.com>
2018-03-14 16:42:49 -0700

    (BOLT-260) Parse parameters from commandline
    This adds parameter parsing for param=val params on the commandline.
    Parameters vals are parsed as JSON and the parsed value is used if it
    matches the declared type for the parameter

--- spec/lib/bolt_spec/pal.rb
+++ spec/lib/bolt_spec/pal.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'bolt_spec/files'
+
 module BoltSpec
   module PAL
     include BoltSpec::Files
@@ -21,10 +23,26 @@ module BoltSpec
       end
     end
 
-    def with_manifest(_code, _name, _type, _moduledir)
-      # TODO: write code to a tempdir on the module path
-      q_name = 'dne'
-      yield q_name
+    def mk_files(path, content)
+      FileUtils.mkdir_p(path)
+      content.each do |name, cont|
+        full_path = File.join(path, name)
+        if cont.is_a? Hash
+          mk_files(full_path, cont)
+        else
+          File.open(full_path, 'w') { |f| f.write(cont) }
+        end
+      end
+    end
+
+    def pal_with_module_content(mods)
+      Dir.mktmpdir do |tmpdir|
+        mk_files(tmpdir, mods)
+        conf = config
+        conf[:modulepath] = tmpdir
+        pal = Bolt::PAL.new(conf)
+        yield pal
+      end
     end
   end
 end
