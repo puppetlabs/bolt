1f5a06384c905428471306ba90a25982ac98da7c
Michael Smith <michael.smith@puppet.com>
2018-03-15 11:02:51 -0700

    (maint) Acceptance fixes for newer platforms and test:gem

--- acceptance/setup/gem/pre-suite/020_install.rb
+++ acceptance/setup/gem/pre-suite/020_install.rb
@@ -7,14 +7,23 @@ test_name "Install Bolt gem" do
   step "Install Bolt gem" do
     install_command = "gem install bolt --source #{gem_source}"
     install_command += " -v '#{gem_version}'" unless gem_version.empty?
-    result = nil
     case bolt['platform']
     when /windows/
       execute_powershell_script_on(bolt, install_command)
-      result = on(bolt, powershell('bolt --help'))
     else
       on(bolt, install_command)
-      result = on(bolt, 'bolt --help')
+    end
+  end
+  step "Ensure install succeeded" do
+    cmd = 'bolt --help'
+    case bolt['platform']
+    when /windows/
+      result = on(bolt, powershell(cmd))
+    when /osx/
+      env = 'source /etc/profile  ~/.bash_profile ~/.bash_login ~/.profile && '
+      result = on(bolt, env + cmd)
+    else
+      result = on(bolt, cmd)
     end
     assert_match(/Usage: bolt <subcommand>/, result.stdout)
   end
