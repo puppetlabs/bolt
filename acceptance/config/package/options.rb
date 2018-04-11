a0c5312a9a50cbce69f1afdf4c9630a27e622c66
Michael Smith <michael.smith@puppet.com>
2018-04-11 10:44:13 -0700

    (maint) Fix package setup for macOS and Windows
    Uses beaker-puppet helper to install Bolt from development builds.

--- acceptance/config/package/options.rb
+++ acceptance/config/package/options.rb
@@ -2,7 +2,6 @@
 
 {
   pre_suite: [
-    'setup/package/pre-suite/015_install_puppet_agent_repo.rb',
     'setup/package/pre-suite/020_install.rb'
   ],
   load_path: './lib/acceptance'
--- acceptance/setup/package/pre-suite/020_install.rb
+++ acceptance/setup/package/pre-suite/020_install.rb
@@ -1,9 +1,39 @@
 # frozen_string_literal: true
 
-step 'Install Bolt package' do
-  bolt_sha = ENV['SHA']
+# Inspired by https://github.com/puppetlabs/pdk/blob/master/package-testing/pre/000_install_package.rb
+# Uses helpers from beaker-puppet to fetch build defaults
 
-  install_puppetlabs_dev_repo(bolt, 'bolt', bolt_sha, 'repo-configs')
-  bolt.install_package('bolt')
-  add_puppet_paths_on(bolt)
+test_name 'Install Bolt package' do
+  if ENV['LOCAL_PKG']
+    pkg = File.basename(ENV['LOCAL_PKG'])
+    step "Install local package #{pkg}" do
+      scp_to(bolt, ENV['LOCAL_PKG'], pkg)
+      # TODO: add Windows::Pkg::install_local_package
+      if bolt['platform'] =~ /windows/
+        generic_install_msi_on(bolt, pkg)
+      else
+        bolt.install_local_package(pkg)
+      end
+    end
+  else
+    dev_builds_url = ENV['DEV_BUILDS_URL'] || 'http://builds.delivery.puppetlabs.net'
+    sha_yaml_url = "#{dev_builds_url}/bolt/#{ENV['SHA']}/artifacts/#{ENV['SHA']}.yaml"
+    unless link_exists?(sha_yaml_url)
+      fail_test("<SHA>.yaml URL '#{sha_yaml_url}' does not exist.")
+    end
+
+    base_url, build_details = fetch_build_details(sha_yaml_url)
+    artifact_url, _ = host_urls(bolt, build_details, base_url)
+
+    case bolt['platform']
+      # TODO: BKR-1109 requests a supported way to install packages on Windows and OSX
+    when %r{windows}
+      generic_install_msi_on(bolt, artifact_url)
+    when %r{osx}
+      on bolt, "curl -O #{artifact_url}"
+      bolt.install_package('bolt*')
+    else
+      bolt.install_package(artifact_url)
+    end
+  end
 end
