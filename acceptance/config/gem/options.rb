4432f255c08d0bea30a3224674091c94d32f228d
Michael Smith <michael.smith@puppet.com>
2018-07-02 16:54:09 -0700

    (BOLT-634) Acceptance tests for apply keyword
    Add acceptance tests for applying a manifest block using facts and vars
    from the target.

--- acceptance/config/gem/options.rb
+++ acceptance/config/gem/options.rb
@@ -5,7 +5,9 @@
     'setup/common/pre-suite/010_install_ruby.rb',
     'setup/gem/pre-suite/020_install.rb',
     'setup/common/pre-suite/030_set_password.rb',
-    'setup/common/pre-suite/050_build_bolt_inventory.rb'
+    'setup/common/pre-suite/050_build_bolt_inventory.rb',
+    'setup/common/pre-suite/070_install_puppet.rb',
+    'setup/common/pre-suite/071_install_facts.rb'
   ],
   load_path: './lib/acceptance'
 }
