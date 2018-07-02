8181141464c92181cb4d9685c0617e59696e37bc
Michael Smith <michael.smith@puppet.com>
2018-07-02 15:36:31 -0700

    (BOLT-634) Add integration tests
    Add a set of fast integration tests that compilation works and invokes
    the apply_catalog task. Will leave actually applying the manifest block
    to acceptance tests, as that will require a puppet-agent install.

---
+++ spec/fixtures/apply/basic/plans/init.pp
@@ -0,0 +1,15 @@
+plan basic(TargetSpec $nodes) {
+  $result = run_plan('facts', nodes => $nodes)
+  if !$result.ok {
+    return $result
+  }
+
+  return apply($nodes) {
+    file { '/root/test/':
+      ensure => directory,
+    } -> file { "/root/test/hello.txt":
+      ensure => file,
+      content => "hi there I'm ${$facts['os']['family']}\n",
+    }
+  }
+}
