f16c05f68d6cc5bcb81eedd0db2fb705fb395c0c
Michael Smith <michael.smith@puppet.com>
2018-07-06 10:05:28 -0700

    (maint) Fix acceptance helpers so Rototiller and beaker subcommands work
    Rototiller expects defaults to be declared in the Rakefile, while beaker
    subcommands don't use the Rakefile. The end of the acceptance README
    provides instructions for running with beaker subcommands that didn't
    work unless you explicitly set all the optional environment variables
    you needed. Refactor so that both paths share defaults.

--- acceptance/setup/git/pre-suite/020_install.rb
+++ acceptance/setup/git/pre-suite/020_install.rb
@@ -1,24 +1,30 @@
 # frozen_string_literal: true
 
+require 'bolt_setup_helper'
+
 test_name "Install Bolt via git" do
+  extend Acceptance::BoltSetupHelper
+
   sha = ''
   version = ''
   step "Clone repo" do
-    on(bolt, "git clone #{ENV['GIT_SERVER']}/#{ENV['GIT_FORK']} bolt")
-    if ENV['GIT_SHA'].empty?
-      on(bolt, "cd bolt && git checkout #{ENV['GIT_BRANCH']}")
+    on(bolt, "git clone #{git_server}/#{git_fork} bolt")
+    if git_sha.empty?
+      on(bolt, "cd bolt && git checkout #{git_branch}")
     else
-      on(bolt, "cd bolt && git checkout #{ENV['GIT_SHA']}")
+      on(bolt, "cd bolt && git checkout #{git_sha}")
     end
   end
+
   step "Update submodules" do
     on(bolt, "cd bolt && git submodule update --init --recursive")
   end
+
   step "Construct version based on SHA" do
-    sha = if ENV['GIT_SHA'].empty?
+    sha = if git_sha.empty?
             on(bolt, 'cd bolt && git rev-parse --short HEAD').stdout.chomp
           else
-            ENV['GIT_SHA']
+            git_sha
           end
     version = "9.9.#{sha}"
     create_remote_file(bolt, 'bolt/lib/bolt/version.rb', <<-VERS)
@@ -27,6 +33,7 @@ test_name "Install Bolt via git" do
     end
     VERS
   end
+
   step "Build gem" do
     build_command = "cd bolt; gem build bolt.gemspec"
     case bolt['platform']
@@ -36,6 +43,7 @@ test_name "Install Bolt via git" do
       on(bolt, build_command)
     end
   end
+
   step "Install custom gem" do
     install_command = "cd bolt; gem install bolt-#{version}.gem --no-ri --no-rdoc"
     case bolt['platform']
@@ -45,6 +53,7 @@ test_name "Install Bolt via git" do
       on(bolt, install_command)
     end
   end
+
   step "Ensure install succeeded" do
     cmd = 'bolt --version'
     case bolt['platform']
