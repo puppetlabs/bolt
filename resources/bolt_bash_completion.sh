bfc234374ba953239a2b21c3d22f277945cf29fe
Marc Schoechlin <marc.schoechlin@vico-research.com>
2018-07-10 09:35:28 +0200

    bash completion
    - add simple bash completion
    - add documentation

---
+++ resources/bolt_bash_completion.sh
@@ -0,0 +1,62 @@
+#####################################################################
+###
+### Bash autocompletion for bolt
+### include this file in your .bash_profile or .bashrc to use completion
+### "source bolt_bash_completion.sh"
+<<<<<<< HEAD
+###
+### Marc Schoechlin ms-github@256bit.org
+=======
+>>>>>>> d69aaca586ea952a62ac99e01839abb081d2e50d
+
+_bolt()
+{
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    local prev=${COMP_WORDS[COMP_CWORD-1]}
+    local next=""
+
+    local all_options="-q, --query --noop --description --params -u, --user -p, --password --private-key --[no-]host-key-check --[no-]ssl --[no-]ssl-verify --run-as --sudo-password -c, --concurrency --modulepath --boltdir --configfile --inventoryfile --transport --connect-timeout --[no-]tty --tmpdir --format --[no-]color -h, --help --verbose --debug --trace --version"
+
+    local all_hosts="$(hget all 2>/dev/null|tr ',' ' ')"
+
+     case $prev in
+      bolt)
+         next="command file task plan"
+		;;
+      command)
+         next="run"
+      ;;
+      file)
+         next="upload"
+      ;;
+      task)
+         next="show run"
+      ;;
+      plan)
+         next="show run"
+      ;;
+      run|upload|show)
+         next=""
+      ;;
+      --nodes|-n)
+<<<<<<< HEAD
+         # TODO: add automatic lookup here, ideally bolt should have a lookup function suitable for this 
+         # to provide completions from the ~/.puppetlabs/bolt/inventory.yaml file
+         next=""
+=======
+         next="$all_hosts"
+>>>>>>> d69aaca586ea952a62ac99e01839abb081d2e50d
+      ;;
+      --query|-q|--description|--params|--user|-u|-p|--password|--private-key|--run-as|--sudo-password|--concurrency|-c|--modulepath|--boltdir|--configfile|--inventoryfile|--transport|--connect-timeout|--tmpdir|--format)
+         next=""
+         ;;
+      *)
+         next="$all_options"
+         ;;
+     esac
+
+    # Sort the options
+    COMPREPLY=( $(compgen -W "$next" -- $cur) )
+}
+
+complete -F _bolt bolt
