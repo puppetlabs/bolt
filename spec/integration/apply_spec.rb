6428e768a138a8f39ab1056fb055251eeaeb1836
Michael Smith <michael.smith@puppet.com>
2018-07-10 14:03:40 -0700

    (BOLT-675) Add tests for vars and trusted data

--- spec/integration/apply_spec.rb
+++ spec/integration/apply_spec.rb
@@ -10,7 +10,7 @@ describe "Passes parsed AST to the apply_catalog task" do
   include BoltSpec::Conn
 
   let(:modulepath) { File.join(__dir__, '../fixtures/apply') }
-  let(:config_flags) { %W[--format json --nodes #{uri} --password #{password} --modulepath #{modulepath}] }
+  let(:config_flags) { %W[--format json --nodes #{uri} --password #{password} --modulepath #{modulepath}] + tflags }
 
   before(:each) do
     allow_any_instance_of(Bolt::Applicator).to receive(:catalog_apply_task) {
@@ -24,9 +24,10 @@ describe "Passes parsed AST to the apply_catalog task" do
     let(:uri) { conn_uri('ssh') }
     let(:password) { conn_info('ssh')[:password] }
     let(:apply_task) { 'apply_catalog.sh' }
+    let(:tflags) { %w[--no-host-key-check] }
 
     it 'echos the catalog ast' do
-      result = run_cli_json(%w[plan run basic --no-host-key-check] + config_flags)
+      result = run_cli_json(%w[plan run basic] + config_flags)
       ast = result[0]['result']
       expect(ast).to be
       expect(ast['catalog_uuid']).to be
@@ -38,15 +39,42 @@ describe "Passes parsed AST to the apply_catalog task" do
       expect(files.count).to eq(1)
       expect(files[0]['parameters']['content']).to match(/hi there I'm Debian/)
     end
+
+    it 'uses trusted facts' do
+      result = run_cli_json(%w[plan run basic::trusted] + config_flags)
+      ast = result[0]['result']
+      notify = ast['resources'].select { |r| r['type'] == 'Notify' }
+      expect(notify.count).to eq(1)
+      expect(notify[0]['title']).to eq(
+        'trusted {authenticated => local, certname => localhost, extensions => {}, hostname => localhost, domain => }'
+      )
+    end
+
+    it 'uses target vars' do
+      result = run_cli_json(%w[plan run basic::target_vars] + config_flags)
+      ast = result[0]['result']
+      notify = ast['resources'].select { |r| r['type'] == 'Notify' }
+      expect(notify.count).to eq(1)
+      expect(notify[0]['title']).to eq('hello there')
+    end
+
+    it 'plan vars override target vars' do
+      result = run_cli_json(%w[plan run basic::plan_vars] + config_flags)
+      ast = result[0]['result']
+      notify = ast['resources'].select { |r| r['type'] == 'Notify' }
+      expect(notify.count).to eq(1)
+      expect(notify[0]['title']).to eq('hello world')
+    end
   end
 
   describe 'over winrm', winrm: true do
     let(:uri) { conn_uri('winrm') }
     let(:password) { conn_info('winrm')[:password] }
     let(:apply_task) { 'apply_catalog.ps1' }
+    let(:tflags) { %w[--no-ssl --no-ssl-verify] }
 
     it 'echos the catalog ast' do
-      result = run_cli_json(%w[plan run basic --no-ssl --no-ssl-verify] + config_flags)
+      result = run_cli_json(%w[plan run basic] + config_flags)
       ast = result[0]['result']
       expect(ast).to be
       expect(ast['catalog_uuid']).to be
@@ -57,6 +85,32 @@ describe "Passes parsed AST to the apply_catalog task" do
       files = resources['File'].select { |f| f['title'] == '/root/test/hello.txt' }
       expect(files.count).to eq(1)
       expect(files[0]['parameters']['content']).to match(/hi there I'm windows/)
+    end
+
+    it 'uses trusted facts' do
+      result = run_cli_json(%w[plan run basic::trusted] + config_flags)
+      ast = result[0]['result']
+      notify = ast['resources'].select { |r| r['type'] == 'Notify' }
+      expect(notify.count).to eq(1)
+      expect(notify[0]['title']).to eq(
+        'trusted {authenticated => local, certname => localhost, extensions => {}, hostname => localhost, domain => }'
+      )
+    end
+
+    it 'uses target vars' do
+      result = run_cli_json(%w[plan run basic::target_vars] + config_flags)
+      ast = result[0]['result']
+      notify = ast['resources'].select { |r| r['type'] == 'Notify' }
+      expect(notify.count).to eq(1)
+      expect(notify[0]['title']).to eq('hello there')
+    end
+
+    it 'plan vars override target vars' do
+      result = run_cli_json(%w[plan run basic::plan_vars] + config_flags)
+      ast = result[0]['result']
+      notify = ast['resources'].select { |r| r['type'] == 'Notify' }
+      expect(notify.count).to eq(1)
+      expect(notify[0]['title']).to eq('hello world')
     end
   end
 end
