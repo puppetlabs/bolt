f6b44ff3867b268d058fcffb2d7d218d02428b82
Michael Smith <michael.smith@puppet.com>
2018-07-12 16:26:34 -0700

    (BOLT-649) Add tests that run-as works with apply
    Work for BOLT-648 added `run-as` support as well. Add tests that it
    works.

--- acceptance/tests/apply_nonroot.rb
+++ acceptance/tests/apply_nonroot.rb
@@ -9,9 +9,28 @@ test_name "bolt plan run with should apply manifest block on remote hosts via ss
   ssh_nodes = select_hosts(roles: ['ssh'])
   skip_test('no applicable nodes to test on') if ssh_nodes.empty?
 
-  dir = bolt.tmpdir('apply_ssh')
+  dir = bolt.tmpdir('apply_nonroot')
   fixtures = File.absolute_path('files')
-  filepath = '/tmp/test'
+  filepath = '/etc/puppetlabs/test'
+  user = 'apply_nonroot'
+
+  step 'create nonroot user on targets' do
+    on(ssh_nodes, puppet('resource', 'user', user, 'ensure=present'))
+
+    teardown do
+      on(ssh_nodes, puppet('resource', 'user', user, 'ensure=absent'))
+    end
+  end
+
+  step 'disable requiretty for root user' do
+    create_remote_file(ssh_nodes, "/etc/sudoers.d/#{user}", <<-FILE)
+Defaults:root !requiretty
+FILE
+
+    teardown do
+      on(ssh_nodes, "rm /etc/sudoers.d/#{user}")
+    end
+  end
 
   step "create plan on bolt controller" do
     on(bolt, "mkdir -p #{dir}/modules/example_apply/plans")
@@ -26,12 +45,8 @@ test_name "bolt plan run with should apply manifest block on remote hosts via ss
     '--format'     => 'json'
   }
 
-  teardown do
-    on(ssh_nodes, "rm -rf #{filepath}")
-  end
-
-  step "execute `bolt plan run noop=true` via SSH with json output" do
-    result = bolt_command_on(bolt, bolt_command + ' noop=true', flags)
+  step "execute `bolt plan run run_as=#{user}` via SSH with json output" do
+    result = bolt_command_on(bolt, bolt_command + " run_as=#{user}", flags)
     assert_equal(0, result.exit_code,
                  "Bolt did not exit with exit code 0")
 
@@ -46,36 +61,12 @@ test_name "bolt plan run with should apply manifest block on remote hosts via ss
       # Verify that node succeeded
       host = node.hostname
       result = json.select { |n| n['node'] == host }
-      assert_equal('success', result[0]['status'],
-                   "The task did not succeed on #{host}")
+      assert_equal('failure', result[0]['status'],
+                   "The task did not fail on #{host}")
+      assert_match(/Permission denied/, result[0]['result']['_error']['msg'])
 
       # Verify that files were not created on the target
       on(node, "cat #{filepath}/hello.txt", acceptable_exit_codes: [1])
-    end
-  end
-
-  step "execute `bolt plan run` via SSH with json output" do
-    result = bolt_command_on(bolt, bolt_command, flags)
-    assert_equal(0, result.exit_code,
-                 "Bolt did not exit with exit code 0")
-
-    begin
-      json = JSON.parse(result.stdout)
-    rescue JSON.ParserError
-      assert_equal("Output should be JSON", result.string,
-                   "Output should be JSON")
-    end
-
-    ssh_nodes.each do |node|
-      # Verify that node succeeded
-      host = node.hostname
-      result = json.select { |n| n['node'] == host }
-      assert_equal('success', result[0]['status'],
-                   "The task did not succeed on #{host}")
-
-      # Verify that files were created on the target
-      content = on(node, "cat #{filepath}/hello.txt")
-      assert_match(/^hi there I'm [a-zA-Z]+$/, content.stdout)
     end
   end
 end
