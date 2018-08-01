bc26c6c1fef98dbedbd911acbcee162c26211815
Michael Smith <michael.smith@puppet.com>
2018-08-01 10:51:54 -0700

    (BOLT-641) Update acceptance tests to use new setup method
    Include `puppet_agent` in acceptance setup so it's available for the
    `apply_prep` call. Also add custom facts to the test.

--- acceptance/setup/common/pre-suite/071_install_modules.rb
+++ acceptance/setup/common/pre-suite/071_install_modules.rb
@@ -7,6 +7,9 @@ test_name "Install modules" do
 
   create_remote_file(bolt, "#{default_boltdir}/Puppetfile", <<-PUPPETFILE)
 mod 'puppetlabs-facts', '0.2.0'
+mod 'puppet_agent',
+    git: 'https://github.com/puppetlabs/puppetlabs-puppet_agent',
+    ref: 'fa0c9e2ecb0cfde1d916fedf97559e2e91daf763'
 PUPPETFILE
 
   bolt_command_on(bolt, 'bolt puppetfile install')
