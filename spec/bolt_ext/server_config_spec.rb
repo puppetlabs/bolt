17c0e3bcaa9613c620699654d6504ef1b67b62c5
Lucy Wyman <wyman.lucy@gmail.com>
2018-08-08 09:56:23 -0700

    (BOLT-654) Add SSL configuration for bolt-server
    This adds 3 keys to the bolt-server configuration:
    * `ssl-cert`: the path to the ssl cert for bolt-server to use
    * `ssl-key`: the path to the ssl key for bolt-server to use
    * `ssl-ca-cert`: the path to the ssl ca certificat for bolt-server to use
    
    It validates that each of these keys is present and a readable file, then configures Puma to bind to the specified port over SSL using these credentials.

--- spec/bolt_ext/server_config_spec.rb
+++ spec/bolt_ext/server_config_spec.rb
@@ -7,6 +7,7 @@ describe TransportConfig do
   let(:emptyconfig) { File.join(__dir__, '..', 'fixtures', 'configs', 'empty-bolt-server.conf') }
   let(:globalconfig) { File.join(__dir__, '..', 'fixtures', 'configs', 'global-bolt-server.conf') }
   let(:localconfig) { File.join(__dir__, '..', 'fixtures', 'configs', 'local-bolt-server.conf') }
+  let(:requiredconfig) { File.join(__dir__, '..', 'fixtures', 'configs', 'required-bolt-server.conf') }
 
   it "reads a port from 'global' config" do
     config = TransportConfig.new(globalconfig)
@@ -16,7 +17,7 @@ describe TransportConfig do
   it "reads a port from local config" do
     # This needs to have the empty config as the global config so that rspec
     # doesn't try to read /etc/puppetlabs/bolt-server/conf.d/bolt-server.conf
-    config = TransportConfig.new(emptyconfig, localconfig)
+    config = TransportConfig.new(requiredconfig, localconfig)
     expect(config.port).to be(6789)
   end
 
@@ -26,7 +27,21 @@ describe TransportConfig do
   end
 
   it "accepts an empty config" do
-    config = TransportConfig.new(emptyconfig)
+    config = TransportConfig.new(requiredconfig)
     expect(config.port).to be(8144)
+  end
+
+  it "reads ssl keys from config" do
+    config = TransportConfig.new(globalconfig)
+    expect(config.ssl_cert).to eq('spec/fixtures/ssl/cert.pem')
+    expect(config.ssl_key).to eq('spec/fixtures/ssl/key.pem')
+    expect(config.ssl_ca_cert).to eq('spec/fixtures/ssl/ca.pem')
+  end
+
+  it "errors when a required key is not present" do
+    expect { TransportConfig.new(emptyconfig) }.to raise_error(Bolt::ValidationError, /You must configure/)
+  end
+
+  it "errors when a specified file does not exist" do
   end
 end
