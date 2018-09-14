afac5dba17451e0961df6e3bb417030e7e373fb9
Lucy Wyman <wyman.lucy@gmail.com>
2018-09-14 07:23:55 -0700

    (maint) Add links to PQL information
    **What this changes** This adds links to more information about PQL to relevant parts of the documentation
    **Why** Because it's not obvious to new users what PQL is or how it's different from other query languages, and providing direct links to PQL information in 'obvious' places is helpful for finding that info

--- bolt-modules/boltlib/lib/puppet/functions/puppetdb_query.rb
+++ bolt-modules/boltlib/lib/puppet/functions/puppetdb_query.rb
@@ -2,12 +2,16 @@
 
 require 'bolt/error'
 
-# Makes a query to puppetdb using Bolt's PuppetDB client.
+# Makes a query to {https://puppet.com/docs/puppetdb/latest/index.html puppetdb}
+# using Bolt's PuppetDB client.
 Puppet::Functions.create_function(:puppetdb_query) do
+  # rubocop:disable Metrics/LineLength
   # @param query A PQL query.
+  #   {https://puppet.com/docs/puppetdb/latest/api/query/tutorial-pql.html Learn more about Puppet's query language, PQL}
   # @return Results of the PuppetDB query.
   # @example Request certnames for all nodes
   #   puppetdb_query('nodes[certname] {}')
+  # rubocop:enable Metrics/LineLength
   dispatch :make_query do
     param 'Variant[String, Array[Data]]', :query
     return_type 'Array[Data]'
