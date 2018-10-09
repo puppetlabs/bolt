9d10345d3279219c9cc516e8ce5eace0006b997b
Michael Smith <michael.smith@puppet.com>
2018-10-09 10:43:51 -0700

    (BOLT-755) Add integration test covering failure modes
    Add integration tests for each transport showing that shareable task
    code works, and how it fails.

---
+++ spec/integration/shareable_task_spec.rb
@@ -0,0 +1,97 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'bolt_spec/integration'
+require 'bolt_spec/conn'
+require 'bolt/util'
+
+shared_examples "invalid metadata" do
+  it 'fails with an unknown file' do
+    expect {
+      run_cli_json(%w[task run shareable::unknown_file] + config_flags)
+    }.to raise_error(Bolt::PAL::PALError, %r{Could not find .*shareable/lib/nope on disk})
+  end
+
+  it 'fails with an unknown module' do
+    expect {
+      run_cli_json(%w[task run shareable::unknown_module] + config_flags)
+    }.to raise_error(Bolt::PAL::PALError, /Could not find module not_a_module containing task file meh/)
+  end
+
+  it 'fails with an invalid path' do
+    msg = /Files must be saved in module directories that Puppet makes available via mount points: lib, files, tasks/
+    expect {
+      run_cli_json(%w[task run shareable::invalid_path] + config_flags)
+    }.to raise_error(Bolt::PAL::PALError, msg)
+  end
+
+  it 'fails with a file referenced as a directory' do
+    msg = if Bolt::Util.windows?
+            'Files specified in task metadata cannot include a trailing slash: ' \
+            'results/lib/puppet/functions/results/make_result.rb/'
+          else
+            %r{Could not find .*results/lib/puppet/functions/results/make_result.rb/ on disk}
+          end
+    expect {
+      run_cli_json(%w[task run shareable::not_a_directory] + config_flags)
+    }.to raise_error(Bolt::PAL::PALError, msg)
+  end
+
+  it 'fails with a directory referenced as a file' do
+    msg = %r{Directories specified in task metadata must include a trailing slash: results/lib/puppet}
+    expect {
+      run_cli_json(%w[task run shareable::not_a_file] + config_flags)
+    }.to raise_error(Bolt::PAL::PALError, msg)
+  end
+end
+
+describe "Shareable tasks with files" do
+  include BoltSpec::Integration
+  include BoltSpec::Conn
+
+  let(:modulepath) { File.join(__dir__, '../fixtures/modules') }
+  let(:config_flags) { %W[--format json --nodes #{target} --modulepath #{modulepath}] + options }
+
+  describe 'over ssh', ssh: true do
+    let(:target) { conn_uri('ssh', include_password: true) }
+    let(:options) { %w[--no-host-key-check] }
+
+    it 'runs a task with multiple files' do
+      result = run_cli_json(%w[task run shareable] + config_flags)
+      files = result['items'][0]['result']['_output'].split("\n").map(&:strip).sort
+      expect(files[0]).to match(%r{^174 .*/_installdir/shareable/tasks/unknown_file.json$})
+      expect(files[1]).to match(%r{^310 .*/_installdir/results/lib/puppet/functions/results/make_result.rb$})
+      expect(files[2]).to match(%r{^43 .*/_installdir/error/tasks/fail.sh$})
+    end
+
+    include_examples "invalid metadata"
+  end
+
+  describe 'over winrm', winrm: true do
+    let(:target) { conn_uri('winrm') }
+    let(:options) { %W[--no-ssl --password #{conn_info('winrm')[:password]}] }
+
+    it 'runs a task with multiple files' do
+      result = run_cli_json(%w[task run shareable] + config_flags)
+      files = result['items'][0]['result']['_output'].split("\n").map(&:strip).sort
+      expect(files).to eq(%w[178 310 43])
+    end
+
+    include_examples "invalid metadata"
+  end
+
+  describe 'over local:bash', bash: true do
+    let(:target) { 'localhost' }
+    let(:options) { [] }
+
+    it 'runs a task with multiple files' do
+      result = run_cli_json(%w[task run shareable] + config_flags)
+      files = result['items'][0]['result']['_output'].split("\n").map(&:strip).sort
+      expect(files[0]).to match(%r{^174 .*/_installdir/shareable/tasks/unknown_file.json$})
+      expect(files[1]).to match(%r{^310 .*/_installdir/results/lib/puppet/functions/results/make_result.rb$})
+      expect(files[2]).to match(%r{^43 .*/_installdir/error/tasks/fail.sh$})
+    end
+
+    include_examples "invalid metadata"
+  end
+end
