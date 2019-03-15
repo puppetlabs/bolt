50a6a02f8f4eced1d9bb08ee09b33cbf6fb77e70
Iristyle <Iristyle@github>
2019-03-15 09:33:57 -0700

    (BOLT-1186) Disable logging plugins
     - The logging plugin loads the LazyPlugger gem to discover gem based
       plugins. Bolt doesn't use this plugin system and it has a fairly
       expensive cost since it performs a gem scan.
    
       Unfortunately this library is not configurable at all since it is an
       implementation detail inside of the Logging library.
    
       Monkey-patch the library so that the plugin system is never
       initialized to reduce the Bolt startup time for simple commands like
       `bolt --help`
    
     - This reduces load time in a controlled Windows environment from
       approximately 2.7 seconds to 2.3 seconds

--- lib/bolt.rb
+++ lib/bolt.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# loads Logging gem, patching it for perf reasons to disable plugins
+require 'logging_extensions/logging'
+
 module Bolt
   require 'bolt/executor'
 end
