6f4c419743d3725a8c393af924bf516253a989d0
Iristyle <Iristyle@github>
2019-03-15 08:49:42 -0700

    (BOLT-1119) Lazy load Docker gem
     - Only load docker gem when it is actually needed in an effort to
       reduce the Bolt startup time for simple commands like `bolt --help`
    
     - This reduces load time in a controlled Windows environment from
       approximately 4.4 seconds to 4.15 seconds, reducing Ruby
       $LOADED_FEATURES from 845 to 790 (a reduction of 55 loaded files)
    
     - This also prevents the gem excon from loading

---
+++ spec/integration/cli_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe "when loading bolt for CLI invocation" do
+  context 'and calling help' do
+    def cli_loaded_features
+      cli_loader = File.join(__dir__, '..', 'fixtures', 'scripts', 'bolt_cli_loader.rb')
+      `bundle exec ruby #{cli_loader}`.split("\n")
+    end
+
+    let(:loaded_features) { cli_loaded_features }
+
+    [
+      'docker-api',
+      'excon'
+    ].each do |gem_name|
+      it "does not load #{gem_name} gem code" do
+        gem_path = Regexp.escape(Gem.loaded_specs[gem_name].full_gem_path)
+        any_gem_source_code = a_string_matching(gem_path)
+        fail_msg = "loaded unexpected #{gem_name} gem code from #{gem_path}"
+        expect(loaded_features).not_to include(any_gem_source_code), fail_msg
+      end
+    end
+  end
+end
