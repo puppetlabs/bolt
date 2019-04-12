319d8ae5cc2ecc237f18a9a22f32d7c285c6abf5
donoghuc <cas.donoghue@gmail.com>
2019-04-12 11:23:40 -0700

    (MODULES-8750) Update facts module to 0.5.1
    Pull in bug fixes from the 0.5.1 release.

--- acceptance/setup/common/pre-suite/071_install_modules.rb
+++ acceptance/setup/common/pre-suite/071_install_modules.rb
@@ -7,7 +7,7 @@ test_name "Install modules" do
 
   on(bolt, "mkdir -p #{default_boltdir}")
   create_remote_file(bolt, "#{default_boltdir}/Puppetfile", <<-PUPPETFILE)
-mod 'puppetlabs-facts', '0.5.0'
+mod 'puppetlabs-facts', '0.5.1'
 mod 'puppetlabs-service', '0.6.0'
 mod 'puppet_agent',
     git: 'https://github.com/puppetlabs/puppetlabs-puppet_agent',
