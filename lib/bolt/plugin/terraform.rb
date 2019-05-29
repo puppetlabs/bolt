4cc663bd2292a24652c5d4385c13e72e75a6aef7
Nick Lewis <nick@puppetlabs.com>
2019-05-29 10:05:15 -0700

    (BOLT-1341) Support Terraform statefile version 4
    Terraform 0.12 introduced version 4 of the statefile format, which is
    significantly different from the previous version. In particular, resources are
    organized differently, named differently, and their parameters are broken into
    nested hashes and arrays instead of alays being a flat hash.
    
    This adds support for the new statefile format by checking the "version" field
    and determining how to process the resources. We still accept exactly the same
    options in the plugin and the user doesn't need to be aware of which format
    their statefile is in.
    
    We do some standardization of the resources into a list, but keep their
    parameters in whatever form they came from the statefile. Then at lookup time,
    we check for both the dotted form of the key as well as doing the nested
    lookup. Only one of those should match at a time, so this prevents us from
    having to keep track of which format the data is in.

--- lib/bolt/plugin/terraform.rb
+++ lib/bolt/plugin/terraform.rb
@@ -24,29 +24,20 @@ module Bolt
       def lookup_targets(opts)
         state = load_statefile(opts)
 
-        resources = state.fetch('modules', {}).flat_map do |mod|
-          mod.fetch('resources', {}).map do |name, resource|
-            [name, resource.dig('primary', 'attributes')]
-          end
-        end
+        resources = extract_resources(state)
 
         regex = Regexp.new(opts['resource_type'])
 
         resources.select do |name, _resource|
           name.match?(regex)
         end.map do |name, resource|
-          unless resource.key?(opts['uri'])
-            warn_missing_property(name, opts['uri'])
-            next
-          end
+          uri = lookup(name, resource, opts['uri'])
+          next unless uri
 
-          target = { 'uri' => resource[opts['uri']] }
+          target = { 'uri' => uri }
           if opts.key?('name')
-            if resource.key?(opts['name'])
-              target['name'] = resource[opts['name']]
-            else
-              warn_missing_property(name, opts['name'])
-            end
+            real_name = lookup(name, resource, opts['name'])
+            target['name'] = real_name if real_name
           end
           if opts.key?('config')
             target['config'] = resolve_config(name, resource, opts['config'])
@@ -65,15 +56,57 @@ module Bolt
         raise Bolt::FileError.new("Could not load Terraform state file #{filename}: #{e}", filename)
       end
 
+      # Format the list of resources into a list of [name, attribute map]
+      # pairs. This method handles both version 4 and earlier statefiles, doing
+      # the appropriate munging based on the shape of the data.
+      def extract_resources(state)
+        if state['version'] >= 4
+          state.fetch('resources', []).flat_map do |resource_set|
+            prefix = "#{resource_set['type']}.#{resource_set['name']}"
+            resource_set['instances'].map do |resource|
+              instance_name = prefix
+              instance_name += ".#{resource['index_key']}" if resource['index_key']
+
+              [instance_name, resource['attributes']]
+            end
+          end
+        else
+          state.fetch('modules', {}).flat_map do |mod|
+            mod.fetch('resources', {}).map do |name, resource|
+              [name, resource.dig('primary', 'attributes')]
+            end
+          end
+        end
+      end
+
+      # Look up a nested value from the resource attributes. The key is of the
+      # form `foo.bar.0.baz`. For terraform statefile version 3, this will
+      # exactly correspond to a key in the resource. In version 4, it will
+      # correspond to a nested hash entry at {foo: {bar: [{baz: <value>}]}}
+      # For simplicity's sake, we just check both.
+      def lookup(name, resource, path)
+        segments = path.split('.').map do |segment|
+          begin
+            Integer(segment)
+          rescue ArgumentError
+            segment
+          end
+        end
+
+        value = resource[path] || resource.dig(*segments)
+        unless value
+          warn_missing_property(name, path)
+        end
+        value
+      end
+
+      # Walk the "template" config mapping provided in the plugin config and
+      # replace all values with the corresponding value from the resource
+      # parameters.
       def resolve_config(name, resource, config_template)
         Bolt::Util.walk_vals(config_template) do |value|
           if value.is_a?(String)
-            if resource.key?(value)
-              resource[value]
-            else
-              warn_missing_property(name, value)
-              nil
-            end
+            lookup(name, resource, value)
           else
             value
           end
--- spec/bolt/plugin/terraform_spec.rb
+++ spec/bolt/plugin/terraform_spec.rb
@@ -6,9 +6,8 @@ require 'bolt/plugin/terraform'
 # rubocop:disable Style/BracesAroundHashParameters
 describe Bolt::Plugin::Terraform do
   let(:terraform_dir) { File.expand_path(File.join(__dir__, '../../fixtures/terraform')) }
-  let(:resource_type) { 'google_compute_instance' }
+  let(:resource_type) { 'google_compute_instance.*' }
   let(:uri) { 'network_interface.0.access_config.0.nat_ip' }
-  let(:opts) { { 'dir' => terraform_dir, 'resource_type' => resource_type, 'uri' => uri } }
 
   it 'has a hook for lookup_targets' do
     expect(subject.hooks).to eq(['lookup_targets'])
@@ -28,43 +27,68 @@ describe Bolt::Plugin::Terraform do
     expect(state).to eq(JSON.parse(File.read(statefile)))
   end
 
-  it 'matches resources that start with the given type' do
-    targets = subject.lookup_targets(opts)
+  shared_examples('loading terraform targets') do
+    let(:opts) do
+      { 'dir' => terraform_dir,
+        'statefile' => statefile,
+        'resource_type' => resource_type,
+        'uri' => uri }
+    end
 
-    expect(targets).to contain_exactly({ 'uri' => '34.83.150.52' }, { 'uri' => '34.83.16.240' })
+    it 'matches resources that start with the given type' do
+      targets = subject.lookup_targets(opts)
+
+      expect(targets).to contain_exactly({ 'uri' => ip0 }, { 'uri' => ip1 })
+    end
+
+    it 'can filter resources by regex' do
+      targets = subject.lookup_targets(opts.merge('resource_type' => 'google_compute_instance.example.\d+'))
+
+      expect(targets).to contain_exactly({ 'uri' => ip0 }, { 'uri' => ip1 })
+    end
+
+    it 'maps inventory to name' do
+      targets = subject.lookup_targets(opts.merge('name' => 'id'))
+
+      expect(targets).to contain_exactly({ 'uri' => ip0, 'name' => 'test-instance-0' },
+                                         { 'uri' => ip1, 'name' => 'test-instance-1' })
+    end
+
+    it 'builds a config map from the inventory' do
+      config_template = { 'ssh' => { 'user' => 'metadata.sshUser' } }
+      targets = subject.lookup_targets(opts.merge('config' => config_template))
+
+      config = { 'ssh' => { 'user' => 'someone' } }
+      expect(targets).to contain_exactly({ 'uri' => ip0, 'config' => config },
+                                         { 'uri' => ip1, 'config' => config })
+    end
+
+    it 'returns nothing if there are no matching resources' do
+      targets = subject.lookup_targets(opts.merge('resource_type' => 'aws_instance'))
+
+      expect(targets).to be_empty
+    end
+
+    it 'fails if the state file does not exist' do
+      expect { subject.lookup_targets(opts.merge('statefile' => 'nonexistent.tfstate')) }
+        .to raise_error(Bolt::Error, /Could not load Terraform state file nonexistent.tfstate/)
+    end
   end
 
-  it 'can filter resources by regex' do
-    targets = subject.lookup_targets(opts.merge('resource_type' => 'google_compute_instance.example.\d+'))
+  describe "using a terrform version 3 state file" do
+    let(:statefile) { 'terraform3.tfstate' }
+    let(:ip0) { '34.83.150.52' }
+    let(:ip1) { '34.83.16.240' }
 
-    expect(targets).to contain_exactly({ 'uri' => '34.83.150.52' }, { 'uri' => '34.83.16.240' })
+    include_examples 'loading terraform targets'
   end
 
-  it 'maps inventory to name' do
-    targets = subject.lookup_targets(opts.merge('name' => 'id'))
+  describe "using a terraform version 4 state file" do
+    let(:statefile) { 'terraform.tfstate' }
+    let(:ip0) { '34.83.160.116' }
+    let(:ip1) { '35.230.3.44' }
 
-    expect(targets).to contain_exactly({ 'uri' => '34.83.150.52', 'name' => 'test-instance-0' },
-                                       { 'uri' => '34.83.16.240', 'name' => 'test-instance-1' })
-  end
-
-  it 'builds a config map from the inventory' do
-    config_template = { 'ssh' => { 'user' => 'metadata.sshUser' } }
-    targets = subject.lookup_targets(opts.merge('config' => config_template))
-
-    config = { 'ssh' => { 'user' => 'someone' } }
-    expect(targets).to contain_exactly({ 'uri' => '34.83.150.52', 'config' => config },
-                                       { 'uri' => '34.83.16.240', 'config' => config })
-  end
-
-  it 'returns nothing if there are no matching resources' do
-    targets = subject.lookup_targets(opts.merge('resource_type' => 'aws_instance'))
-
-    expect(targets).to be_empty
-  end
-
-  it 'fails if the state file does not exist' do
-    expect { subject.lookup_targets(opts.merge('statefile' => 'nonexistent.tfstate')) }
-      .to raise_error(Bolt::Error, /Could not load Terraform state file nonexistent.tfstate/)
+    include_examples 'loading terraform targets'
   end
 end
 # rubocop:enable Style/BracesAroundHashParameters
