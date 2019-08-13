a71e2f86744f8703ba4bf730a759239b005545be
Lucy Wyman <wyman.lucy@gmail.com>
2019-08-13 13:30:43 -0700

    (GH-1145) Remove sudo-password with no run-as warning
    Closes #1145
    
    This removes a warning that's raised when `--sudo-password` is specified
    on the command line when `--run-as` is not specified on the command
    line. The warning was often raised for valid use cases where
    `--sudo-password` was specified and `--run-as` was not, and predicting
    whether the warning was needed is more difficult than the warning is
    worth.

--- lib/bolt/transport/sudoable.rb
+++ lib/bolt/transport/sudoable.rb
@@ -6,12 +6,7 @@ require 'bolt/transport/base'
 module Bolt
   module Transport
     class Sudoable < Base
-      def self.validate_sudo_options(options, logger)
-        if options['sudo-password'] && options['run-as'].nil?
-          logger.warn("--sudo-password will not be used without specifying a " \
-                      "user to escalate to with --run-as")
-        end
-
+      def self.validate_sudo_options(options)
         run_as_cmd = options['run-as-command']
         if run_as_cmd && (!run_as_cmd.is_a?(Array) || run_as_cmd.any? { |n| !n.is_a?(String) })
           raise Bolt::ValidationError, "run-as-command must be an Array of Strings, received #{run_as_cmd}"
