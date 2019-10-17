dc9f0c911e0dad5e247c8cc4a45a8da3d2ac3889
Alex Dreyer <alex@puppetlabs.com>
2019-10-17 11:42:07 -0700

    (GH-1270) Move limit to be a keyword option

--- bolt-modules/ctrl/lib/puppet/functions/ctrl/do_until.rb
+++ bolt-modules/ctrl/lib/puppet/functions/ctrl/do_until.rb
@@ -2,24 +2,30 @@
 
 # Repeat the block until it returns a truthy value. Returns the value.
 Puppet::Functions.create_function(:'ctrl::do_until') do
+  # @param options Additional options: 'until'
   # @example Run a task until it succeeds
-  #   ctrl::do_until(10) || {
+  #   ctrl::do_until() || {
   #     run_task('test', $target, _catch_errors => true).ok()
   #   }
+  #
+  # @example Run a task until it succeeds or fails 10 times
+  #   ctrl::do_until('limit' => 10) || {
+  #     run_task('test', $target, _catch_errors => true).ok()
+  #   }
+  #
   dispatch :do_until do
-    optional_param 'Integer', :limit
+    optional_param 'Hash[String[1], Any]', :options
     block_param
   end
 
-  def do_until(limit)
+  def do_until(options = { 'limit' => 0 })
     Puppet.lookup(:bolt_executor) {}&.report_function_call(self.class.name)
-    i=0
-    limit ||= 0
+    limit = options['limit']
+    i = 0
     until (x = yield)
-     i = i+1
-     next if limit==0
-     break if i >= limit
+      i += 1
+      break if limit != 0 && i >= limit
     end
-    return x
+    x
   end
 end
