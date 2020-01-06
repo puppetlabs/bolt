e23bbbf23a0c40654a0b6f406a5e721b78447efd
Lucy Wyman <wyman.lucy@gmail.com>
2020-01-06 14:23:04 -0800

    (GH-1159) Rename bolt-server docker artifacts
    Having the bolt-server Dockerfile at the root of the Bolt repo is
    confusing for users who are doing the hands-on lab and find this
    Dockerfile by mistake instead of the hands-on lab one. This renames
    Dockerfile to Dockerfile.bolt-server and docker-compose to
    docker-compose-bolt-server in order to clarify that they're used for
    bolt-server and not bolt.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,6 +9,6 @@ BOLT_VERSION=`git describe`
 BOLT_TAG=$REGISTRY_HOST/$DOCKER_REGISTRY/bolt-server:$BOLT_VERSION
 LATEST_TAG=$REGISTRY_HOST/$DOCKER_REGISTRY/bolt-server:latest
 
-docker build --tag $BOLT_TAG --tag $LATEST_TAG --build-arg bolt_version=$BOLT_VERSION ./
+docker build --tag $BOLT_TAG --tag $LATEST_TAG --build-arg bolt_version=$BOLT_VERSION -f ./Dockerfile.bolt-server .
 docker push $BOLT_TAG
 docker push $LATEST_TAG
