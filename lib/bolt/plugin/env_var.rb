a336b57a15c33aab5dbaf770b784b5c0f562bd93
op-ct <chris.tessmer@onyxpoint.com>
2020-04-27 13:49:34 -0400

    Update lib/bolt/plugin/env_var.rb
    Co-Authored-By: beechtom <30373575+beechtom@users.noreply.github.com>

--- lib/bolt/plugin/env_var.rb
+++ lib/bolt/plugin/env_var.rb
@@ -17,7 +17,7 @@ module Bolt
         unless opts['var']
           raise Bolt::ValidationError, "env_var plugin requires that the 'var' is specified"
         end
-        return if opts['optional']
+        return if opts['optional'] || opts['default']
         unless ENV[opts['var']]
           raise Bolt::ValidationError, "env_var plugin requires that the var '#{opts['var']}' be set"
         end
