6c3e0a942bf220d16937d6f9b1a3406edbb3da97
James Pogran <jpogran@outlook.com>
2021-01-11 15:37:04 -0500

    (GH-2551) Move to bolt.bat
    This removes the `bolt` PowerShell function and instead relies on the bolt.bat file written in the bolt packaging. This enables Bolt to be used in any shell on Windows while still retaining PowerShell functionality.
    
    !feature
    
    * **Move to use bolt.bat for execution on Windows** ([#2551](https://github.com/puppetlabs/bolt/issues/2551))
    
       This removes the `bolt` PowerShell function and instead relies on the bolt.bat file written in the bolt packaging.

--- pwsh_module/pwsh_bolt_internal.ps1
+++ pwsh_module/pwsh_bolt_internal.ps1
@@ -1,43 +1,13 @@
-function bolt {
-  $fso = New-Object -ComObject Scripting.FileSystemObject
-
-  $script:BOLT_BASEDIR = (Get-ItemProperty -Path "HKLM:\Software\Puppet Labs\Bolt").RememberedInstallDir
-  # Windows API GetShortPathName requires inline C#, so use COM instead
-  $script:BOLT_BASEDIR = $fso.GetFolder($script:BOLT_BASEDIR).ShortPath
-  $script:RUBY_DIR = $script:BOLT_BASEDIR
-
-  if (!$env:SSL_CERT_FILE) {
-    $env:SSL_CERT_FILE = "$($script:BOLT_BASEDIR)\ssl\cert.pem"
-  }
-  if (!$env:SSL_CERT_DIR) {
-    $env:SSL_CERT_DIR = "$($script:BOLT_BASEDIR)\ssl\certs"
-  }
-
-  &$script:RUBY_DIR\bin\ruby -S -- $script:RUBY_DIR\bin\bolt ($args -replace '"', '"""')
-}
-
 function Invoke-BoltCommandline {
   [CmdletBinding()]
   param($params)
-  $fso = New-Object -ComObject Scripting.FileSystemObject
-
   $script:BOLT_BASEDIR = (Get-ItemProperty -Path "HKLM:\Software\Puppet Labs\Bolt").RememberedInstallDir
-  # Windows API GetShortPathName requires inline C#, so use COM instead
-  $script:BOLT_BASEDIR = $fso.GetFolder($script:BOLT_BASEDIR).ShortPath
-  $script:RUBY_DIR = $script:BOLT_BASEDIR
+  
+  $env:PATH = "$($script:BOLT_BASEDIR)\bin;" + $env:PATH
 
-  if (!$env:SSL_CERT_FILE) {
-    $env:SSL_CERT_FILE = "$($script:BOLT_BASEDIR)\ssl\cert.pem"
-  }
-  if (!$env:SSL_CERT_DIR) {
-    $env:SSL_CERT_DIR = "$($script:BOLT_BASEDIR)\ssl\certs"
-  }
+  Write-Verbose "Executing bolt $($params -join ' ')"
 
-  $processArgs = @('-S', '--', "$script:RUBY_DIR\bin\bolt") + $params
-
-  Write-Verbose "Executing $($script:RUBY_DIR)\bin\ruby $($processArgs -join ' ')"
-
-  &$script:RUBY_DIR\bin\ruby $processArgs
+  bolt $params
 }
 
 function Get-BoltCommandline {
