90be1825691e657b0f9c093773cb624f9c2d5a40
Enis Inan <enis.inan@puppet.com>
2021-01-26 08:50:41 -0800

    (PE-31004) Support input data env var for the PuppetConnectData plugin
    This commit lets users specify the path to a YAML file containing their
    PuppetConnect input data via the PUPPET_CONNECT_INPUT_DATA environment
    variable. The difference between this and puppet_connect_data.yaml is
    that the input data file's meant to be copy-pasted as-is into Puppet
    Connect's UI (hence requiring additional validation), while
    puppet_connect_data.yaml's meant to contain the relevant, non-autoloaded
    secrets required to get the user's inventory file working with Bolt (so it
    should contain only plugin references).
    
    !no-release-note
    
    Signed-off-by: Enis Inan <enis.inan@puppet.com>

--- lib/bolt/plugin/puppet_connect_data.rb
+++ lib/bolt/plugin/puppet_connect_data.rb
@@ -3,12 +3,45 @@
 module Bolt
   class Plugin
     class PuppetConnectData
+      INPUT_DATA_VAR = 'PUPPET_CONNECT_INPUT_DATA'
+
       def initialize(context:, **_opts)
-        puppet_connect_data_yaml_path = File.join(context.boltdir, 'puppet_connect_data.yaml')
+        if ENV.key?(INPUT_DATA_VAR)
+          # The user provided input data that they will copy-paste into the Puppet Connect UI
+          # for inventory syncing. This environment variable will likely be set when invoking a
+          # general "test Puppet Connect input data" command. That command tests that parsing
+          # the inventory with the given input data results in connectable targets. Part of
+          # that requires validating that the input data contains all of the referenced keys,
+          # which is what this plugin will do in validate_resolve_reference.
+          @input_data_path = ENV[INPUT_DATA_VAR]
+          data_path = @input_data_path
+        else
+          # The user is using this plugin during a regular Bolt invocation, so fetch the (minimal)
+          # required data from the default location. This data should typically be non-autoloadable
+          # secrets like WinRM passwords.
+          #
+          # Note that any unspecified keys will be resolved to nil.
+          data_path = File.join(context.boltdir, 'puppet_connect_data.yaml')
+        end
+
         @data = Bolt::Util.read_optional_yaml_hash(
-          puppet_connect_data_yaml_path,
-          'puppet_connect_data.yaml'
+          data_path,
+          File.basename(data_path)
         )
+
+        if @input_data_path
+          # Validate that the data does not contain any plugin-reference
+          # values
+          @data.each do |key, toplevel_value|
+            # Use walk_vals to check for nested plugin references
+            Bolt::Util.walk_vals(toplevel_value) do |current_value|
+              if current_value.is_a?(Hash) && current_value.key?('_plugin')
+                raise invalid_input_data_err("the #{key} key's value contains a plugin reference")
+              end
+              current_value
+            end
+          end
+        end
       end
 
       def name
@@ -29,6 +62,15 @@ module Bolt
           raise Bolt::ValidationError,
                 "puppet_connect_data plugin requires that 'key' be specified"
         end
+        if @input_data_path && !@data.key?(opts['key'])
+          # Input data for Puppet Connect was provided and opts['key'] does not have a
+          # value specified. Raise an error for this case.
+          raise invalid_input_data_err("a value for the #{opts['key']} key is not specified")
+        end
+      end
+
+      def invalid_input_data_err(msg)
+        Bolt::ValidationError.new("invalid input data #{@input_data_path}: #{msg}")
       end
     end
   end
