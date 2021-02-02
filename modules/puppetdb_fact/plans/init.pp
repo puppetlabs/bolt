2d9eca6719ab41be7fc72cab7159e169f4dfb5de
Tom Beech <beech.tom@gmail.com>
2021-02-02 13:43:02 -0800

    (GH-2565) Add plan metadata to `aggregate` and `puppetdb_fact` plans
    This adds plan metadata to the plans in the `aggregate` and
    `puppetdb_fact` modules, which will show in `bolt plan show <plan>` and
    `Get-BoltPlan -Name <plan>` output.
    
    This also removes the `aggregate::nodes` plan, which has been superseded
    by `aggregate::targets`.
    
    !removal
    
    * **Remove `aggregate::nodes` plan**
    
      Bolt no longer ships with the `aggregate::nodes` plan. Use the
      `aggregate::targets` plan instead.

--- modules/puppetdb_fact/plans/init.pp
+++ modules/puppetdb_fact/plans/init.pp
@@ -1,3 +1,13 @@
+# @summary
+#   Collect facts for the specified targets from PuppetDB and store them
+#   on the Targets.
+#
+# This plan accepts a list of targets to collect facts for from the configured
+# PuppetDB connection. After collecting facts, they are stored on each target's
+# Target object. The updated facts can then be accessed using `$target.facts`.
+#
+# @param targets
+#   The targets to collect facts for.
 plan puppetdb_fact(TargetSpec $targets) {
   $targs = get_targets($targets)
   $certnames = $targs.map |$target| { $target.host }
