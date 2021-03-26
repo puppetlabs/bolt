35e78b4d2db0e6730d547255c67097b48256cf28
Tom Beech <beech.tom@gmail.com>
2021-03-26 09:06:26 -0700

    (hack) Autogenerate type reference doc
    This adds puppet-strings documentation to Bolt's built-in data types and
    adds some missing tags for built-in functions. With the addition of
    puppet-strings to Bolt's data types, the type reference page is now
    autogenerated.
    
    !no-release-note

--- bolt-modules/boltlib/lib/puppet/datatypes/containerresult.rb
+++ bolt-modules/boltlib/lib/puppet/datatypes/containerresult.rb
@@ -1,5 +1,32 @@
 # frozen_string_literal: true
 
+# The [run_container](plan_functions.md#run_container) plan function returns a
+# `ContainerResult` object. A `ContainerResult` is a standalone object (not part
+# of a `ResultSet`) that includes either the `stdout` and `stderr` values from
+# running the container, or an `_error` object if the container exited with a
+# nonzero exit code.
+#
+# @param value
+#   A hash including the `stdout`, `stderr`, and `exit_code` received from the
+#   container.
+#
+# @!method []
+#   Accesses the value hash directly and returns the value for the key. This
+#   function does not use dot notation. Call the function directly on the
+#   `ContainerResult`. For example, `$result[key]`.
+# @!method error
+#   An object constructed from the `_error` field of the result's value.
+# @!method ok
+#   Whether the result was successful.
+# @!method status
+#   Either `success` if the result was successful or `failure`.
+# @!method stdout
+#   The value of 'stdout' output by the container.
+# @!method stderr
+#   The value of 'stderr' output by the container.
+# @!method to_data
+#   A serialized representation of `ContainerResult`.
+#
 Puppet::DataTypes.create_type('ContainerResult') do
   interface <<-PUPPET
     attributes => {
