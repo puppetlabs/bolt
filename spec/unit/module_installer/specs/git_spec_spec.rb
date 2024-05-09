puppetlabs/bolt

commit 0a090696cb5807640e894fdbc0d6158edf70184f

(BOLT-1589) Support installing any git-based module
This updates Bolt to support installing git-based modules from any
source. Now, Bolt will attempt to download metadata directly from GitHub
or GitLab for public repositories and fall back to cloning the git
repo's metadata if that fails.

This also updates the spec resolver to no longer calculate a SHA based
on the ref provided in project configuration. Previously, Bolt would
calculate a SHA and write it to the Puppetfile to pin the module to a
specific commit when modules are installed. However, doing this is
difficult and time-consuming with the move to supporting any git-based
module.

!feature

* **Support installing any git-based module**
  ([#3109](https://github.com/puppetlabs/bolt/issues/3109))

  Bolt now supports installing any git-based module. Previously, Bolt
  only supported installing and resolving dependencies for modules
  hosted in a public GitHub repository. Now, Bolt will check both GitHub
  and GitLab before falling back to cloning a module's metadata using
  the `git` executable. This allows users to install modules from
  private repositories, or from locations other than GitHub.

lib/bolt/module_installer/specs/git_spec.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 require 'json'
+require 'net/http'
+require 'open3'
 require 'set'
 
 require_relative '../../../bolt/error'
+require_relative '../../../bolt/logger'
 
 # This class represents a Git module specification.
 #
@@ -18,11 +21,17 @@ module Bolt
         attr_reader :git, :ref, :resolve, :type
 
         def initialize(init_hash)
-          @resolve    = init_hash.key?('resolve') ? init_hash['resolve'] : true
-          @name       = parse_name(init_hash['name'])
-          @git, @repo = parse_git(init_hash['git'])
-          @ref        = init_hash['ref']
-          @type       = :git
+          @logger  = Bolt::Logger.logger(self)
+          @resolve = init_hash.key?('resolve') ? init_hash['resolve'] : true
+          @git     = init_hash['git']
+          @ref     = init_hash['ref']
+          @name    = parse_name(init_hash['name'])
+          @type    = :git
+
+          unless @resolve == true || @resolve == false
+            raise Bolt::ValidationError,
+                  "Option 'resolve' for module spec #{@git} must be a Boolean"
+          end
 
           if @name.nil? && @resolve == false
             raise Bolt::ValidationError,
@@ -30,9 +39,9 @@ module Bolt
                   "must include a 'name' key when 'resolve' is false."
           end
 
-          unless @resolve == true || @resolve == false
+          unless valid_url?(@git)
             raise Bolt::ValidationError,
-                  "Option 'resolve' for module spec #{@git} must be a Boolean"
+                  "Invalid URI #{@git}. Valid URIs must begin with 'git@', 'http://', or 'https://'."
           end
         end
 
@@ -57,22 +66,6 @@ module Bolt
           match[:name]
         end
 
-        # Gets the repo from the git URL.
-        #
-        private def parse_git(git)
-          return [git, nil] unless @resolve
-
-          repo = if git.start_with?('git@github.com:')
-                   git.split('git@github.com:').last.split('.git').first
-                 elsif git.start_with?('https://github.com')
-                   git.split('https://github.com/').last.split('.git').first
-                 else
-                   raise Bolt::ValidationError, invalid_git_msg(git)
-                 end
-
-          [git, repo]
-        end
-
         # Returns true if the specification is satisfied by the module.
         #
         def satisfied_by?(mod)
@@ -88,14 +81,6 @@ module Bolt
           }
         end
 
-        # Returns an error message that the provided repo is not a git repo or
-        # is private.
-        #
-        private def invalid_git_msg(repo_name)
-          "#{repo_name} is not a public GitHub repository. See https://pup.pt/no-resolve "\
-            "for information on how to install this module."
-        end
-
         # Returns a PuppetfileResolver::Model::GitModule object for resolving.
         #
         def to_resolver_module
@@ -103,94 +88,167 @@ module Bolt
 
           PuppetfileResolver::Puppetfile::GitModule.new(name).tap do |mod|
             mod.remote = @git
-            mod.ref    = sha
+            mod.ref    = @ref
           end
         end
 
         # Resolves the module's title from the module metadata. This is lazily
-        # resolved since Bolt does not always need to know a Git module's name.
+        # resolved since Bolt does not always need to know a Git module's name,
+        # and fetching the metadata to figure it out is expensive.
         #
         def name
-          @name ||= begin
-            url      = "https://raw.githubusercontent.com/#{@repo}/#{sha}/metadata.json"
-            response = make_request(:Get, url)
+          @name ||= parse_name(metadata['name'])
+        end
 
-            case response
-            when Net::HTTPOK
-              body = JSON.parse(response.body)
+        # Fetches the module's metadata. Attempts to fetch metadata from either
+        # GitHub or GitLab and falls back to cloning the repo if that fails.
+        #
+        private def metadata
+          data = github_metadata || gitlab_metadata || clone_metadata
 
-              unless body.key?('name')
-                raise Bolt::Error.new(
-                  "Missing name in metadata.json at #{git}. This is not a valid module.",
-                  "bolt/missing-module-name-error"
-                )
+          unless data
+            raise Bolt::Error.new(
+              "Unable to locate metadata.json for module at #{@git}. This may not be a valid module. "\
+              "For more information about how Bolt attempted to locate the metadata file, check the "\
+              "debugging logs.",
+              'bolt/missing-module-metadata-error'
+            )
+          end
+
+          data = JSON.parse(data)
+
+          unless data.is_a?(Hash)
+            raise Bolt::Error.new(
+              "Invalid metadata.json at #{@git}. Expected a Hash, got a #{data.class}.",
+              'bolt/invalid-module-metadata-error'
+            )
+          end
+
+          unless data.key?('name')
+            raise Bolt::Error.new(
+              "Invalid metadata.json at #{@git}. Metadata must include a 'name' key.",
+              'bolt/missing-module-name-error'
+            )
+          end
+
+          data
+        rescue JSON::ParserError => e
+          raise Bolt::Error.new(
+            "Unable to parse metadata.json for module at #{@git}: #{e.message}",
+            'bolt/metadata-parse-error'
+          )
+        end
+
+        # Returns the metadata for a GitHub-hosted module.
+        #
+        private def github_metadata
+          repo = if @git.start_with?('git@github.com:')
+                   @git.split('git@github.com:').last.split('.git').first
+                 elsif @git.start_with?('https://github.com')
+                   @git.split('https://github.com/').last.split('.git').first
+                 end
+
+          return nil if repo.nil?
+
+          request_metadata("https://raw.githubusercontent.com/#{repo}/#{@ref}/metadata.json")
+        end
+
+        # Returns the metadata for a GitLab-hosted module.
+        #
+        private def gitlab_metadata
+          repo = if @git.start_with?('git@gitlab.com:')
+                   @git.split('git@gitlab.com:').last.split('.git').first
+                 elsif @git.start_with?('https://gitlab.com')
+                   @git.split('https://gitlab.com/').last.split('.git').first
+                 end
+
+          return nil if repo.nil?
+
+          request_metadata("https://gitlab.com/#{repo}/-/raw/#{@ref}/metadata.json")
+        end
+
+        # Returns the metadata by cloning a git-based module.
+        # Because cloning is the last attempt to locate module metadata
+        #
+        private def clone_metadata
+          unless git?
+            @logger.debug("'git' executable not found, unable to use git clone resolution.")
+            return nil
+          end
+
+          # Clone the repo into a temp directory that will be automatically cleaned up.
+          Dir.mktmpdir do |dir|
+            command = %W[git clone --bare --depth=1 --single-branch --branch=#{@ref} #{@git} #{dir}]
+            @logger.debug("Executing command '#{command.join(' ')}'")
+
+            out, err, status = Open3.capture3(*command)
+
+            unless status.success?
+              @logger.debug("Unable to clone #{@git}: #{err}")
+              return nil
+            end
+
+            # Read the metadata.json file from the cloned repo.
+            Dir.chdir(dir) do
+              command = %W[git show #{@ref}:metadata.json]
+              @logger.debug("Executing command '#{command.join(' ')}'")
+
+              out, err, status = Open3.capture3(*command)
+
+              unless status.success?
+                @logger.debug("Unable to read metadata.json file for #{@git}: #{err}")
+                return nil
               end
 
-              parse_name(body['name'])
-            else
-              raise Bolt::Error.new(
-                "Missing metadata.json at #{git}. This is not a valid module.",
-                "bolt/missing-module-metadata-error"
-              )
+              out
             end
           end
         end
 
-        # Resolves the SHA for the specified ref. This is lazily resolved since
-        # Bolt does not always need to know a Git module's SHA.
+        # Requests module metadata from the specified url.
         #
-        def sha
-          @sha ||= begin
-            url      = "https://api.github.com/repos/#{@repo}/commits/#{ref}"
-            headers  = ENV['GITHUB_TOKEN'] ? { "Authorization" => "token #{ENV['GITHUB_TOKEN']}" } : {}
-            response = make_request(:Get, url, headers)
+        private def request_metadata(url)
+          uri  = URI.parse(url)
+          opts = { use_ssl: uri.scheme == 'https' }
 
-            case response
-            when Net::HTTPOK
-              body = JSON.parse(response.body)
-              body['sha']
-            when Net::HTTPUnauthorized
-              raise Bolt::Error.new(
-                "Invalid token at GITHUB_TOKEN, unable to resolve git modules.",
-                "bolt/invalid-git-token-error"
-              )
-            when Net::HTTPForbidden
-              message = "GitHub API rate limit exceeded, unable to resolve git modules. "
-
-              unless ENV['GITHUB_TOKEN']
-                message += "To increase your rate limit, set the GITHUB_TOKEN environment "\
-                          "variable with a GitHub personal access token."
-              end
-
-              raise Bolt::Error.new(message, 'bolt/github-api-rate-limit-error')
-            when Net::HTTPNotFound
-              raise Bolt::Error.new(invalid_git_msg(git), "bolt/missing-git-repository-error")
-            else
-              raise Bolt::Error.new(
-                "Ref #{ref} at #{git} is not a commit, tag, or branch.",
-                "bolt/invalid-git-ref-error"
-              )
-            end
-          end
-        end
-
-        # Makes a generic HTTP request.
-        #
-        private def make_request(verb, url, headers = {})
-          require 'net/http'
-
-          uri      = URI.parse(url)
-          opts     = { use_ssl: uri.scheme == 'https' }
+          @logger.debug("Requesting metadata file from #{url}")
 
           Net::HTTP.start(uri.host, uri.port, opts) do |client|
-            request = Net::HTTP.const_get(verb).new(uri, headers)
-            client.request(request)
+            response = client.request(Net::HTTP::Get.new(uri))
+
+            case response
+            when Net::HTTPOK
+              response.body
+            else
+              @logger.debug("Unable to locate metadata file at #{url}")
+              nil
+            end
           end
         rescue StandardError => e
           raise Bolt::Error.new(
             "Failed to connect to #{uri}: #{e.message}",
             "bolt/http-connect-error"
           )
+        end
+
+        # Returns true if the 'git' executable is available.
+        #
+        private def git?
+          Open3.capture3('git', '--version')
+          true
+        rescue Errno::ENOENT
+          false
+        end
+
+        # Returns true if the URL is valid.
+        #
+        private def valid_url?(url)
+          return true if url.start_with?('git@')
+
+          uri = URI.parse(url)
+          uri.is_a?(URI::HTTP) && uri.host
+        rescue URI::InvalidURIError
+          false
         end
       end
     end

spec/unit/module_installer/specs/git_spec_spec.rb
@@ -38,10 +38,10 @@ describe Bolt::ModuleInstaller::Specs::GitSpec do
     end
 
     it 'errors with invalid git source' do
-      init_hash['git'] = 'https://gitlab.com/puppetlabs/puppetlabs-yaml'
+      init_hash['git'] = 'gitlab.com/puppetlabs/puppetlabs-yaml'
       expect { spec }.to raise_error(
         Bolt::ValidationError,
-        /^.*is not a public GitHub repository/
+        /Invalid URI #{init_hash['git']}/
       )
     end
 
@@ -105,10 +105,7 @@ describe Bolt::ModuleInstaller::Specs::GitSpec do
 
   context '#to_resolver_module' do
     it 'returns a puppetfile-resolver module object' do
-      allow(spec).to receive(:sha).and_return(ref)
-      mod = spec.to_resolver_module
-
-      expect(mod).to be_a(PuppetfileResolver::Puppetfile::GitModule)
+      expect(spec.to_resolver_module).to be_a(PuppetfileResolver::Puppetfile::GitModule)
     end
   end
 
@@ -119,75 +116,42 @@ describe Bolt::ModuleInstaller::Specs::GitSpec do
       expect(spec.name).to eq(name)
     end
 
-    context 'with missing metadata.json' do
-      let(:git) { 'https://github.com/puppetlabs/bolt' }
+    it 'errors with missing metadata.json' do
+      allow(spec).to receive(:github_metadata).and_return(nil)
+      allow(spec).to receive(:gitlab_metadata).and_return(nil)
+      allow(spec).to receive(:clone_metadata).and_return(nil)
 
-      it 'errors' do
-        expect { spec.name }.to raise_error(
-          Bolt::Error,
-          /Missing metadata\.json/
-        )
-      end
-    end
-  end
-
-  context '#resolve_sha' do
-    context 'with a valid commit' do
-      let(:ref) { '79f98ffd3faf8d3badb1084a676e5fc1cbac464e' }
-
-      it 'resolves and returns a SHA' do
-        expect(spec.sha).to eq('79f98ffd3faf8d3badb1084a676e5fc1cbac464e')
-      end
-    end
-
-    context 'with a valid tag' do
-      let(:ref) { '0.2.0' }
-
-      it 'resolves and returns a SHA' do
-        expect(spec.sha).to eq('79f98ffd3faf8d3badb1084a676e5fc1cbac464e')
-      end
-    end
-
-    context 'with a valid branch' do
-      let(:ref) { 'main' }
-
-      it 'resolves and returns a SHA' do
-        expect(spec.sha).to be_a(String)
-      end
-    end
-
-    context 'with an invalid ref' do
-      let(:ref) { 'foobar' }
-
-      it 'errors' do
-        expect { spec.sha }.to raise_error(
-          Bolt::Error,
-          /not a commit, tag, or branch/
-        )
-      end
-    end
-
-    context 'with an invalid repository' do
-      let(:git) { 'https://github.com/puppetlabs/foobarbaz' }
-
-      it 'errors' do
-        expect { spec.sha }.to raise_error(
-          Bolt::Error,
-          /is not a public GitHub repository/
-        )
-      end
-    end
-
-    it 'errors with an invalid GitHub token' do
-      original = ENV['GITHUB_TOKEN']
-      ENV['GITHUB_TOKEN'] = 'foo'
-
-      expect { spec.sha }.to raise_error(
+      expect { spec.name }.to raise_error(
         Bolt::Error,
-        /Invalid token at GITHUB_TOKEN/
+        /Unable to locate metadata\.json/
       )
-    ensure
-      ENV['GITHUB_TOKEN'] = original
+    end
+
+    it 'errors with unparseable metadata.json' do
+      allow(spec).to receive(:github_metadata).and_return('{"foo":bar}')
+
+      expect { spec.name }.to raise_error(
+        Bolt::Error,
+        /Unable to parse metadata\.json/
+      )
+    end
+
+    it 'errors when metadata is not a hash' do
+      allow(spec).to receive(:github_metadata).and_return('"foo"')
+
+      expect { spec.name }.to raise_error(
+        Bolt::Error,
+        /Invalid metadata\.json.*Expected a Hash/
+      )
+    end
+
+    it 'errors when metadata is missing a name key' do
+      allow(spec).to receive(:github_metadata).and_return('{}')
+
+      expect { spec.name }.to raise_error(
+        Bolt::Error,
+        /Invalid metadata\.json.*must include a 'name' key/
+      )
     end
   end
 end

bolt.gemspec
@@ -57,7 +57,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency "net-ssh-krb", "~> 0.5"
   spec.add_dependency "orchestrator_client", "~> 0.5"
   spec.add_dependency "puppet", ">= 6.18.0"
-  spec.add_dependency "puppetfile-resolver", "~> 0.5"
+  spec.add_dependency "puppetfile-resolver", ">= 0.6.2", "< 1.0"
   spec.add_dependency "puppet-resource_api", ">= 1.8.1"
   spec.add_dependency "puppet-strings", "~> 2.3"
   spec.add_dependency "r10k", "~> 3.10"

lib/bolt/module_installer/resolver.rb
@@ -88,7 +88,7 @@ module Bolt
             modules << Bolt::ModuleInstaller::Puppetfile::GitModule.new(
               spec.name,
               spec.git,
-              spec.sha
+              spec.ref
             )
           end
         end

spec/integration/module_installer/module_installer_spec.rb
@@ -102,6 +102,106 @@ describe 'installing modules' do
     end
   end
 
+  context 'resolving git modules' do
+    let(:project_config) { { 'modules' => [mod] } }
+
+    context 'public GitHub source' do
+      let(:mod) do
+        {
+          'git' => 'https://github.com/puppetlabs/puppetlabs-yaml.git',
+          'ref' => '0.1.0'
+        }
+      end
+
+      it 'installs the module' do
+        result = run_cli_json(command, project: project)
+
+        expect(result).to eq(
+          'success'    => true,
+          'puppetfile' => project.puppetfile.to_s,
+          'moduledir'  => project.managed_moduledir.to_s
+        )
+        expect(project.puppetfile.exist?).to be(true)
+        expect(project.managed_moduledir.exist?).to be(true)
+
+        puppetfile_content = File.read(project.puppetfile)
+
+        expect(puppetfile_content.lines).to include(
+          /mod 'yaml'/,
+          %r{git: 'https://github.com/puppetlabs/puppetlabs-yaml.git'},
+          /ref: '0.1.0'/
+        )
+      end
+    end
+
+    context 'public GitLab source' do
+      let(:mod) do
+        {
+          'git' => 'https://gitlab.com/simp/pupmod-simp-crypto_policy.git',
+          'ref' => '0.1.0'
+        }
+      end
+
+      it 'installs the module' do
+        result = run_cli_json(command, project: project)
+
+        expect(result).to eq(
+          'success'    => true,
+          'puppetfile' => project.puppetfile.to_s,
+          'moduledir'  => project.managed_moduledir.to_s
+        )
+        expect(project.puppetfile.exist?).to be(true)
+        expect(project.managed_moduledir.exist?).to be(true)
+
+        puppetfile_content = File.read(project.puppetfile)
+
+        expect(puppetfile_content.lines).to include(
+          /mod 'crypto_policy'/,
+          %r{git: 'https://gitlab.com/simp/pupmod-simp-crypto_policy.git'},
+          /ref: '0.1.0'/
+        )
+      end
+    end
+
+    context 'git clone source' do
+      let(:mod) do
+        {
+          'git' => 'https://github.com/puppetlabs/puppetlabs-yaml.git',
+          'ref' => '0.1.0'
+        }
+      end
+
+      it 'installs the module' do
+        # Make Bolt believe this module needs to be cloned since private repos
+        # require authorization that the test runner doesn't have.
+        allow_any_instance_of(Bolt::ModuleInstaller::Specs::GitSpec)
+          .to receive(:github_metadata)
+          .and_return(nil)
+        allow_any_instance_of(Bolt::ModuleInstaller::Specs::GitSpec)
+          .to receive(:gitlab_metadata)
+          .and_return(nil)
+
+        result = run_cli_json(command, project: project)
+
+        expect(result).to eq(
+          'success'    => true,
+          'puppetfile' => project.puppetfile.to_s,
+          'moduledir'  => project.managed_moduledir.to_s
+        )
+        expect(project.puppetfile.exist?).to be(true)
+        expect(project.managed_moduledir.exist?).to be(true)
+
+        puppetfile_content = File.read(project.puppetfile)
+
+        expect(puppetfile_content.lines).to include(
+          /mod 'yaml'/,
+          %r{git: 'https://github.com/puppetlabs/puppetlabs-yaml.git'},
+          /ref: '0.1.0'/
+        )
+      end
+    end
+  end
+
   context 'with forge and git modules' do
     let(:project_config) do
       {
@@ -134,7 +234,7 @@ describe 'installing modules' do
       expect(puppetfile_content.lines).to include(
         /mod 'ruby_task_helper'/,
         %r{git: 'https://github.com/puppetlabs/puppetlabs-ruby_task_helper'},
-        /ref: '23520d05ef8e3f9e1327804bc7d2e1bba33d1df9'/,
+        /ref: '0.4.0'/,
         %r{mod 'puppetlabs/yaml', '0.1.0'}
       )
     end
@@ -210,7 +310,7 @@ describe 'installing modules' do
     it 'errors' do
       expect { run_cli(command, project: project) }.to raise_error(
         Bolt::Error,
-        %r{https://github.com/puppetlabs/puppetlabs-foobarbaz is not a public GitHub repository.}
+        /Unable to locate metadata\.json.*This may not be a valid module/
       )
     end
   end

commit 7407b14ed2bedb2ad72366b95e00ee9ba727b4cf

Allow ssh transport protocol for a git module spec
!bug

* **Allow ssh transport protocol** ([#3237](https://github.com/puppetlabs/bolt/issues/3237))

  Fix regression introduced by commit [0a09069](https://github.com/puppetlabs/bolt/commit/0a090696cb5807640e894fdbc0d6158edf70184f).
  Allow install module from git private repository with SSH transport URI (e.g ssh://git@mygitlabserver:12345/namespace/myprivatemod.git)

lib/bolt/module_installer/specs/git_spec.rb
@@ -45,7 +45,7 @@ module Bolt
 
           unless valid_url?(@git)
             raise Bolt::ValidationError,
-                  "Invalid URI #{@git}. Valid URIs must begin with 'git@', 'http://', or 'https://'."
+                  "Invalid URI #{@git}. Valid URIs must begin with 'git@', 'http://', 'https://' or 'ssh://'."
           end
         end
 
@@ -140,7 +140,7 @@ module Bolt
           return true if url.start_with?('git@')
 
           uri = URI.parse(url)
-          uri.is_a?(URI::HTTP) && uri.host
+          (uri.is_a?(URI::HTTP) || uri.scheme == "ssh") && uri.host
         rescue URI::InvalidURIError
           false
         end

spec/unit/module_installer/specs/git_spec_spec.rb
@@ -61,6 +61,11 @@ describe Bolt::ModuleInstaller::Specs::GitSpec do
         /Option 'resolve'.*must be a Boolean/
       )
     end
+
+    it 'allow ssh URI' do
+      init_hash['git'] = 'ssh://myuser@github.com:12345/puppetlabs/puppetlabs-yaml'
+      expect { spec }.not_to raise_error
+    end
   end
 
   context '#to_hash' do