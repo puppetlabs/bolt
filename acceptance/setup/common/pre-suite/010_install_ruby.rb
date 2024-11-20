8af87dd5bfd53aa972da6ed78e347df0671be7f4
alex501212 <alexthepatterson@hotmail.com>
2024-11-20 18:52:33 +0000

    update install ruby gems for ruby3

--- acceptance/setup/common/pre-suite/010_install_ruby.rb
+++ acceptance/setup/common/pre-suite/010_install_ruby.rb
@@ -26,28 +26,28 @@ PS
       bolt.add_env_var('PATH', "/cygdrive/c/tools/msys64:PATH")
       # public_suffix for win requires Ruby version >= 2.6
       # current Ruby 2.5.0 works with public_suffix version 4.0.7
-      on(bolt, powershell('gem install public_suffix -v 4.0.7'))
+      on(bolt, powershell('gem install public_suffix -v 6.0.1'))
       # current Ruby 2.5.0 works with puppet-strings 2.9.0
-      on(bolt, powershell('gem install puppet-strings -v 2.9.0'))
+      on(bolt, powershell('gem install puppet-strings -v 4.1.3'))
       # net-ssh 7.x no longer supports ruby 2.5
-      on(bolt, powershell('gem install net-ssh -v 6.1.0'))
+      on(bolt, powershell('gem install net-ssh -v 7.3.0'))
       # semantic puppet no longer supports ruby < 2.7
-      on(bolt, powershell('gem install semantic_puppet -v 1.0.4'))
-      on(bolt, powershell('gem install puppet -v 7.24.0'))
-      on(bolt, powershell('gem install highline -v 2.1.0'))
+      on(bolt, powershell('gem install semantic_puppet -v 1.1.0'))
+      on(bolt, powershell('gem install puppet -v 8.10.0'))
+      on(bolt, powershell('gem install highline -v 3.1.1'))
     when /debian|ubuntu/
       # install system ruby packages
       install_package(bolt, 'ruby')
       install_package(bolt, 'ruby-ffi')
       on(bolt, 'gem install fast_gettext -v 2.4.0')
       # semantic puppet no longer supports ruby < 2.7
-      on(bolt, 'gem install semantic_puppet -v 1.0.4')
-      on(bolt, 'gem install puppet -v 7.24.0')
-      on(bolt, 'gem install highline -v 2.1.0')
-      on(bolt, 'gem install nori -v 2.6.0')
-      on(bolt, 'gem install CFPropertyList -v 3.0.6')
-      on(bolt, 'gem install winrm -v 2.3.6')
-      on(bolt, 'gem install public_suffix -v 5.1.1')
+      on(bolt, 'gem install semantic_puppet -v 1.1.0')
+      on(bolt, 'gem install puppet -v 8.10.0')
+      on(bolt, 'gem install highline -v 3.1.1')
+      on(bolt, 'gem install nori -v 2.7.1')
+      on(bolt, 'gem install CFPropertyList -v 3.0.7')
+      on(bolt, 'gem install winrm -v 2.3.9')
+      on(bolt, 'gem install public_suffix -v 6.0.1')
     when /el-|centos/
       # install system ruby packages
       install_package(bolt, 'ruby')
@@ -55,7 +55,7 @@ PS
       install_package(bolt, 'rubygem-ffi')
       install_package(bolt, 'rubygem-bigdecimal')
       install_package(bolt, 'rubygem-io-console')
-      on(bolt, 'gem install highline -v 2.1.0')
+      on(bolt, 'gem install highline -v 3.1.1')
     when /fedora/
       # install system ruby packages
       install_package(bolt, 'ruby')
@@ -67,22 +67,21 @@ PS
       install_package(bolt, 'rubygem-json')
       install_package(bolt, 'rubygem-bigdecimal')
       install_package(bolt, 'rubygem-io-console')
-      on(bolt, 'gem install highline -v 2.1.0')
+      on(bolt, 'gem install highline -v 3.1.1')
     when /osx/
       # System ruby for osx is 2.3. winrm-fs and its dependencies require > 2.3.
-      on(bolt, 'gem install nori -v 2.6.0 --no-document')
-      on(bolt, 'gem install winrm -v 2.3.6 --no-document')
-      on(bolt, 'gem install winrm-fs -v 1.3.3 --no-document')
-      on(bolt, 'gem install public_suffix -v 5.1.1 --no-document')
-      on(bolt, 'gem install CFPropertyList -v 3.0.6 --no-document')
+      on(bolt, 'gem install nori -v 2.7.0 --no-document')
+      on(bolt, 'gem install winrm -v 2.3.9 --no-document')
+      on(bolt, 'gem install winrm-fs -v 1.3.5 --no-document')
+      on(bolt, 'gem install public_suffix -v 6.0.1 --no-document')
+      on(bolt, 'gem install CFPropertyList -v 3.0.7 --no-document')
       on(bolt, 'gem install fast_gettext -v 2.4.0')
       # System ruby for osx12 is 2.6, which can only manage puppet-strings 2.9.0
-      on(bolt, 'gem install puppet-strings -v 2.9.0 --no-document')
+      on(bolt, 'gem install puppet-strings -v 4.1.3 --no-document')
       # semantic puppet no longer supports ruby < 2.7
-      on(bolt, 'gem install semantic_puppet -v 1.0.4')
-      on(bolt, 'gem install puppet -v 7.24.0')
-      on(bolt, 'gem install highline -v 2.1.0')
-      on(bolt, 'gem install faraday-net_http -v 3.0.2')
+      on(bolt, 'gem install semantic_puppet -v 1.1.0')
+      on(bolt, 'gem install puppet -v 8.10.0')
+      on(bolt, 'gem install highline -v 3.1.1')
     else
       fail_test("#{bolt['platform']} not currently a supported bolt controller")
     end
